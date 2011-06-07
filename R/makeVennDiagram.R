4cc4909c13ccfb21ac55a24758eb09811ea5dbd7
Lihua Julie Zhu <julie.zhu@umassmed.edu>
2011-06-07 20:21:45 +0000

    added the capability to plot 4-way venn diagram in makeVennDiagram 06-07-2011
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ChIPpeakAnno@56024 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/makeVennDiagram.R
+++ R/makeVennDiagram.R
@@ -92,8 +92,102 @@ function(Peaks, NameOfPeaks, maxgap=0,  totalTest, cex=1.5, counts.col="red")
 		vennDiagram(a2, names = NameOfPeaks)
 		list(p.value.1vs2 = p.value.1vs2, p.value.1vs3 = p.value.1vs3, p.value.2vs3 = p.value.2vs3, vennCounts=a2)
 	}
+	else if(n1 == 4)
+        {
+		a1 = cbind(c(0, 0, 0,0,0, 0, 0,0,1, 1,1,1,1, 1,1,1), c(0, 0,1,1,0, 0,1,1,0, 0,1,1, 0,0,1, 1),c(0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1), c(0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1))
+		colnames(a1) = NameOfPeaks
+		a2 = vennCounts(a1)
+		x1 = findVennCounts(list(Peaks[[1]],Peaks[[2]]), NameOfPeaks=NameOfPeaks[1:2], maxgap=maxgap,
+						totalTest=totalTest)
+		p.value.1vs2 = x1$p.value
+		counts1 = x1$vennCounts
+		p1.and.p2 = counts1[4,3]
+		
+		x2 = findVennCounts(list(Peaks[[1]],Peaks[[3]]), NameOfPeaks=c(NameOfPeaks[1], NameOfPeaks[3]), maxgap=maxgap,
+						totalTest=totalTest)
+		p.value.1vs3 = x2$p.value
+		counts2 = x2$vennCounts	
+		p1.and.p3 = counts2[4,3]
+		
+		x3 = findVennCounts(list(Peaks[[2]],Peaks[[3]]), NameOfPeaks=NameOfPeaks[2:3], maxgap=maxgap,
+						totalTest=totalTest)
+		p.value.2vs3 = x3$p.value
+		counts3 = x3$vennCounts
+		p2.and.p3 = counts3[4,3]
+		
+		x4 = findVennCounts(list(Peaks[[3]],Peaks[[4]]), NameOfPeaks=NameOfPeaks[3:4], maxgap=maxgap,
+						totalTest=totalTest)
+		p.value.3vs4 = x4$p.value
+		counts4 = x4$vennCounts
+		p3.and.p4 = counts4[4,3]
+		
+		x5 = findVennCounts(list(Peaks[[1]],Peaks[[4]]), NameOfPeaks=c(NameOfPeaks[1],NameOfPeaks[4]), maxgap=maxgap,totalTest=totalTest)
+		p.value.1vs4 = x5$p.value
+		counts5 = x5$vennCounts
+		p1.and.p4 = counts5[4,3]
+		
+		x6 = findVennCounts(list(Peaks[[2]],Peaks[[4]]), NameOfPeaks=c(NameOfPeaks[2],NameOfPeaks[4]), maxgap=maxgap,totalTest=totalTest)
+		p.value.2vs4 = x6$p.value
+		counts6 = x6$vennCounts
+		p2.and.p4 = counts6[4,3]		
+		
+		overlappingPeaks123 = findOverlappingPeaks(
+				findOverlappingPeaks(Peaks[[1]],Peaks[[2]],NameOfPeaks1 = NameOfPeaks[1], NameOfPeaks2=NameOfPeaks[2], maxgap=maxgap, multiple=F)$Peaks1withOverlap,
+				Peaks[[3]], NameOfPeaks1 = NameOfPeaks[1], NameOfPeaks2=NameOfPeaks[3],maxgap=maxgap, multiple=F)
+		p1.and.p2.and.p3 = length(unique(overlappingPeaks123$OverlappingPeaks[[NameOfPeaks[1]]]))
+		
+		overlappingPeaks124 = findOverlappingPeaks(
+				findOverlappingPeaks(Peaks[[1]],Peaks[[2]],NameOfPeaks1 = NameOfPeaks[1], NameOfPeaks2=NameOfPeaks[2], maxgap=maxgap, multiple=F)$Peaks1withOverlap,
+				Peaks[[4]], NameOfPeaks1 = NameOfPeaks[1], NameOfPeaks2=NameOfPeaks[4],maxgap=maxgap, multiple=F)$OverlappingPeaks
+		p1.and.p2.and.p4 = length(unique(overlappingPeaks124[[NameOfPeaks[1]]]))
+		
+		overlappingPeaks234 = findOverlappingPeaks(
+				findOverlappingPeaks(Peaks[[2]],Peaks[[3]],NameOfPeaks1 = NameOfPeaks[2], NameOfPeaks2=NameOfPeaks[3], maxgap=maxgap, multiple=F)$Peaks1withOverlap,
+				Peaks[[4]], NameOfPeaks1 = NameOfPeaks[2], NameOfPeaks2=NameOfPeaks[4],maxgap=maxgap, multiple=F)$OverlappingPeaks
+		p2.and.p3.and.p4 = length(unique(overlappingPeaks234[[NameOfPeaks[2]]]))
+		
+		overlappingPeaks134 = findOverlappingPeaks(
+				findOverlappingPeaks(Peaks[[1]],Peaks[[3]],NameOfPeaks1 = NameOfPeaks[1], NameOfPeaks2=NameOfPeaks[3], maxgap=maxgap, multiple=F)$Peaks1withOverlap,
+				Peaks[[4]], NameOfPeaks1 = NameOfPeaks[1], NameOfPeaks2=NameOfPeaks[4],maxgap=maxgap, multiple=F)$OverlappingPeaks
+		p1.and.p3.and.p4 = length(unique(overlappingPeaks134[[NameOfPeaks[1]]]))
+				
+		overlappingPeaks1234 = findOverlappingPeaks(overlappingPeaks123$Peaks1withOverlap,
+				Peaks[[4]], NameOfPeaks1 = NameOfPeaks[1], NameOfPeaks2=NameOfPeaks[4],maxgap=maxgap, multiple=F)$OverlappingPeaks
+		p1.and.p2.and.p3.and.p4 = length(unique(overlappingPeaks1234[[NameOfPeaks[1]]]))
+		
+		p1 = length(rownames(Peaks[[1]]))
+		p2 = length(rownames(Peaks[[2]]))
+		p3 = length(rownames(Peaks[[3]]))
+		p4 = length(rownames(Peaks[[4]]))
+
+		p1only = p1 - p1.and.p2 - p1.and.p3 + p1.and.p2.and.p3 - p1.and.p4 + p1.and.p2.and.p4 + p1.and.p3.and.p4 - p1.and.p2.and.p3.and.p4
+		p2only =  p2 - p1.and.p2 - p2.and.p3 + p1.and.p2.and.p3 - p2.and.p4 + p2.and.p3.and.p4 + p1.and.p2.and.p4 - p1.and.p2.and.p3.and.p4
+		p3only =  p3 - p1.and.p3 - p2.and.p3 + p1.and.p2.and.p3 - p3.and.p4 + p2.and.p3.and.p4 + p1.and.p3.and.p4 - p1.and.p2.and.p3.and.p4
+		p4only =  p4 - p1.and.p4 - p2.and.p4 + p1.and.p2.and.p4 - p3.and.p4 + p2.and.p3.and.p4 + p1.and.p3.and.p4 - p1.and.p2.and.p3.and.p4
+		
+		neither1234 = totalTest - ( p1 + p2 - p1.and.p2 + p3only + p4only + p3.and.p4
+				- p1.and.p3.and.p4 - p2.and.p3.and.p4 + p1.and.p2.and.p3.and.p4)
+		Counts =c(neither1234, p4only, p3only, p3.and.p4 - p1.and.p3.and.p4 - p2.and.p3.and.p4 + p1.and.p2.and.p3.and.p4, p2only, p2.and.p4 - p2.and.p3.and.p4 - p1.and.p2.and.p4 + p1.and.p2.and.p3.and.p4, p2.and.p3 - p2.and.p3.and.p4 - p1.and.p2.and.p3 + p1.and.p2.and.p3.and.p4, 
+p2.and.p3.and.p4 - p1.and.p2.and.p3.and.p4, p1only,
+		p1.and.p4 - p1.and.p3.and.p4 - p1.and.p2.and.p4 + p1.and.p2.and.p3.and.p4,
+		p1.and.p3 - p1.and.p3.and.p4 - p1.and.p2.and.p3 + p1.and.p2.and.p3.and.p4,
+		p1.and.p3.and.p4 - p1.and.p2.and.p3.and.p4,
+		p1.and.p2 - p1.and.p2.and.p3 - p1.and.p2.and.p4 + p1.and.p2.and.p3.and.p4,
+		p1.and.p2.and.p4 - p1.and.p2.and.p3.and.p4,
+		p1.and.p2.and.p3 - p1.and.p2.and.p3.and.p4,
+		p1.and.p2.and.p3.and.p4
+		)
+		a2[,5] = Counts	
+		a = cbind(a2[,5], a2[,1:4])
+		colnames(a)[1]="num"
+		#vennDiagram(a2, names = NameOfPeaks)
+		rownames(a)=c("0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010",
+		 "1011", "1100", "1101", "1110", "1111")
+		gplots:::drawVennDiagram(a)
+		list(p.value.1vs2 = p.value.1vs2, p.value.1vs3 = p.value.1vs3, p.value.2vs3 = p.value.2vs3, p.value.1vs4= p.value.1vs4, p.value.2vs4= p.value.2vs4, p.value.3vs4= p.value.3vs4, vennCounts=a2)
+	}	
 	else
 	{
-		stop("Larger than 3 lists are not implemented yet")
+		stop("Larger than 4 lists are not implemented yet")
 	}
 }
