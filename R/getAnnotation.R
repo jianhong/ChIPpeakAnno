dfa670b595746d230a87bdf956b13b78b64be369
Lihua Julie Zhu <julie.zhu@umassmed.edu>
2011-08-29 16:36:51 +0000

    Added transcript option in getAnnotation
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ChIPpeakAnno@57736 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/getAnnotation.R
+++ R/getAnnotation.R
@@ -1,5 +1,5 @@
 getAnnotation <-
-function(mart, featureType=c("TSS","miRNA", "Exon", "5utr", "3utr", "ExonPlusUtr"))
+function(mart, featureType=c("TSS","miRNA", "Exon", "5utr", "3utr", "ExonPlusUtr", "transcript"))
 {
 		featureType = match.arg(featureType)
 		if (missing(mart) || class(mart) !="Mart")
@@ -29,17 +29,20 @@ function(mart, featureType=c("TSS","miRNA", "Exon", "5utr", "3utr", "ExonPlusUtr
 			
 			TSS = TSS[!is.na(TSS[,3]),]
 		}
-	#	TSS = getBM(c("ensembl_gene_id","go_biological_process_id", "go_cellular_component_id", "go_molecular_function_id"), mart = mart)
 		else if (featureType == "ExonPlusUtr")
 		{
 			TSS = getBM(c('ensembl_exon_id','chromosome_name', 'exon_chrom_start','exon_chrom_end', 
 				'strand', 'ensembl_gene_id', '5_utr_start', '5_utr_end','3_utr_start','3_utr_end', 'description'), mart=mart)
 		}
+		 else if (featureType == "transcript")
+                {
+			TSS =getBM(attributes = c("ensembl_transcript_id", "chromosome_name", "transcript_start", "transcript_end", 'strand', 'description', 'ensembl_gene_id'), mart=mart)
+		}
 		TSS = unique(TSS)
 		TSS = TSS[order(TSS[,3]),]		
 		duplicatedID = TSS[duplicated(TSS[,1]),1]
 		TSS = TSS[!duplicated(TSS[,1]),]
-		if (length(duplicatedID) >0)
+		if (length(duplicatedID) >0 && featureType != "3utr" && featureType != "5utr" && featureType != "ExonPlusUtr")
 		{
 			warning("Following duplicated IDs found, only one of entries of the duplicated id will be returned!")
 			warning(duplicatedID)
@@ -48,7 +51,8 @@ function(mart, featureType=c("TSS","miRNA", "Exon", "5utr", "3utr", "ExonPlusUtr
 		{
 			RangedData(IRanges(start=as.numeric(as.character(TSS[,3])),
 					end = as.numeric(as.character(TSS[,4])),
-					names= as.character(TSS[,1])),
+					names= paste(as.character(TSS[,1]), as.character(TSS[,7]), as.character(TSS[,9]), sep="_")),
+					ensembl_exon_id = as.character(TSS[,1]),
 					ensembl_gene_id = as.character(TSS[,6]),
 					utr5start =  as.numeric(as.character(TSS[,7])),
 					utr5end =  as.numeric(as.character(TSS[,8])),
@@ -58,9 +62,16 @@ function(mart, featureType=c("TSS","miRNA", "Exon", "5utr", "3utr", "ExonPlusUtr
 					description=as.character(TSS[,11]),
 					space = as.character(TSS[,2]))
 		}
+		else if (featureType == "transcript")
+		{
+			RangedData(IRanges(start=as.numeric(TSS[,3]), end=as.numeric(TSS[,4]), names= as.character(TSS[,1])), strand = TSS[,5],   description=as.character(TSS[,6]), space = as.character(TSS[,2]), ensembl_gene_id=as.character(TSS[,7]))
+		}
+		else if (featureType == "5utr" || featureType == "3utr")
+		{
+			RangedData(IRanges(start=as.numeric(TSS[,3]), end=as.numeric(TSS[,4]), names= paste(as.character(TSS[,1]), as.character(TSS[,3]), sep="_")), strand = TSS[,5],   description=as.character(TSS[,6]), space = as.character(TSS[,2]), ensembl_transcript_id = as.character(TSS[,1]))
+		}
 		else
 		{
-			RangedData(IRanges(start=as.numeric(TSS[,3]), end=as.numeric(TSS[,4]), names= as.character(TSS[,1])),
-          strand = TSS[,5],   description=as.character(TSS[,6]), space = as.character(TSS[,2]))
+			RangedData(IRanges(start=as.numeric(TSS[,3]), end=as.numeric(TSS[,4]), names= as.character(TSS[,1])), strand = TSS[,5],   description=as.character(TSS[,6]), space = as.character(TSS[,2]))
 		}
 }
