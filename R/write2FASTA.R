15ad81d39d2a5b46878adfe4c5a45a91ff5326ab
Lihua Julie Zhu <julie.zhu@umassmed.edu>
2012-05-13 16:55:50 +0000

    fix write2FASTA on May 13 2012
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ChIPpeakAnno@65860 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/write2FASTA.R
+++ R/write2FASTA.R
@@ -4,13 +4,5 @@ function(mySeq, file="", width=80)
 	descriptions=rownames(mySeq)
 	sequences=mySeq$sequence
 	names(sequences) = descriptions
-	#numF = length(descriptions)
-	
-	#ff = lapply(seq_len(numF),function(i)
-	{
-	#	desc <- descriptions[i]
-        #seq <- sequences[i]
-        #list(desc = desc, seq = seq)
-    #})
 	write.XStringSet(as(sequences,"XStringSet"), file, width=width)
 }
