de17e459ec711c52cc3249b246f987ec69a533d3
Lihua Julie Zhu <julie.zhu@umassmed.edu>
2012-12-05 22:15:22 +0000

    BED2RangedData and GFF2RangedData now accepts files as well as data frame Dec5th2012 by Julie Zhu
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ChIPpeakAnno@71677 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/BED2RangedData.R
+++ R/BED2RangedData.R
@@ -1,11 +1,18 @@
-BED2RangedData <- function(data.BED,header=FALSE)
+BED2RangedData <- function(data.BED,header=FALSE, ...)
 {
-	if (missing(data.BED) || (class(data.BED) != "data.frame") || dim(data.BED)[2] <3)
+	if (missing(data.BED)){
+		stop("data.BED is required!")
+	}
+	if (inherits(data.BED, "character")){
+		data.BED <- read.delim(data.BED, header=header, ...)
+		header <- FALSE
+	}
+	if ((class(data.BED) != "data.frame") || dim(data.BED)[2] <3)
 	{
-		stop("No valid data.BED passed in, which is a data frame as BED format file with at least 3 fields in the order of: chromosome, start and end. Optional fields are name, score and strand etc. Please refer to http://genome.ucsc.edu/FAQ/FAQformat#format1 for details.")
+		stop("No valid data.BED passed in, which is a data frame or file as BED format with at least 3 fields in the order of: chromosome, start and end. Optional fields are name, score and strand etc. Please refer to http://genome.ucsc.edu/FAQ/FAQformat#format1 for details.  If it is a file, please make sure the file path is correct!")
 	}
 	if (header == TRUE)
-	{	
+	{  
 		myPeak = data.BED[-1,]
 	}
 	else
@@ -15,14 +22,14 @@ BED2RangedData <- function(data.BED,header=FALSE)
 	if (dim(myPeak)[2] >=4)
 	{
 		names = as.character(myPeak[,4])
-		if (length(unique(names))==1)
+		if (any(duplicated(names)))
 		{
-			  names = formatC(1:dim(myPeak)[1], width=nchar(dim(myPeak)[1]), flag='0')
+			names = formatC(1:dim(myPeak)[1], width=nchar(dim(myPeak)[1]), flag='0')
 		}
 	}
 	else
 	{
-		 names = formatC(1:dim(myPeak)[1], width=nchar(dim(myPeak)[1]), flag='0')
+		names = formatC(1:dim(myPeak)[1], width=nchar(dim(myPeak)[1]), flag='0')
 	}
 	if (dim(myPeak)[2] >= 5)
 	{		
@@ -35,6 +42,12 @@ BED2RangedData <- function(data.BED,header=FALSE)
 	if (dim(myPeak)[2] >= 6)
 	{		
 		strand = as.character(myPeak[,6])
+		strand.allowed.characters=c("1","-1","+","-")
+		strand.levels=levels(as.factor(strand))
+		if(any(!strand.levels %in% strand.allowed.characters))
+		{
+			stop("only 1, -1, + and - are acceptable for strand info. Please double check you BED data")
+		}
 		strand[strand== "1"] = "+"
 		strand[strand== "-1"] = "-"
 	}
--- R/GFF2RangedData.R
+++ R/GFF2RangedData.R
@@ -1,8 +1,15 @@
-GFF2RangedData <- function(data.GFF,header=FALSE)
+GFF2RangedData <- function(data.GFF,header=FALSE, ...)
 {
-	if (missing(data.GFF) || (class(data.GFF) != "data.frame") || dim(data.GFF)[2] != 9)
+	if (missing(data.GFF)){
+		stop("data.GFF is required!")
+	}
+	if (inherits(data.GFF, "character")){
+		data.GFF <- read.delim(data.GFF, header=header, ...)
+		header <- FALSE
+	}
+	if ((class(data.GFF) != "data.frame") || dim(data.GFF)[2] != 9)
 	{
-		stop("No valid data.GFF passed in, which is a data frame as GFF format with 9 required fields. Please refer to http://genome.ucsc.edu/FAQ/FAQformat#format3 for details.")
+		stop("No valid data.GFF passed in, which is a data frame or file as GFF format with 9 required fields. Please refer to http://genome.ucsc.edu/FAQ/FAQformat#format3 for details. If it is a file, please make sure the file path is correct!")
 	}
 	if (header == TRUE)
 	{	
@@ -13,6 +20,12 @@ GFF2RangedData <- function(data.GFF,header=FALSE)
 		myPeak = data.GFF
 	}	
 	strand = as.character(myPeak[,7])
+	strand.allowed.characters=c("1","-1","+","-")
+	strand.levels=levels(as.factor(strand))
+	if(any(!strand.levels %in% strand.allowed.characters))
+	{
+		stop("only 1, -1, +, - and . are acceptable for strand info. Please double check you GFF data. If strand entries contain '.', please convert it into '+' or '-'.")
+	}
 	strand[strand== "1"] = "+"
 	strand[strand=="-1"] = "-"
 	names = formatC(1:dim(myPeak)[1], width=nchar(dim(myPeak)[1]), flag='0')
