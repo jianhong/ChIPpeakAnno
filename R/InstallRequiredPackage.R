1b52f2b65044b22e46c6c4916718feaf97d3505a
aiminy <jlfmssm@gmail.com>
2016-09-11 16:08:01 -0400

    add function

---
+++ R/InstallRequiredPackage.R
@@ -0,0 +1,14 @@
+#' InstallRequiredPackage
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' InstallRequiredPackage()
+#'
+InstallRequiredPackage <- function() {
+  source("http://bioconductor.org/biocLite.R")
+  biocLite("GenomicRanges")
+  devtools::install_github("nsheff/LOLA")
+
+}
