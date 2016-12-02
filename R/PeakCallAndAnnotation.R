8a47475eaa9c7fc61a7fc85bd822cc01ffd2266f
aiminy <jlfmssm@gmail.com>
2016-12-01 19:34:06 -0500

    add BamFileSortIndexVisualization2

--- R/PeakCallAndAnnotation.R
+++ R/PeakCallAndAnnotation.R
@@ -82,106 +82,78 @@ PeakCallAndAnnotation <- function(input.file.dir,output.file.dir,genome) {
 
 AnnotatePeak3(paste0(temp3,"/"),paste0(output.dir.name,"_PeakAnnotation"),
                 genome="Hs")
-   
-  #sample.name<-sapply(strsplit(names(file.name.3),split="_peaks_"),"[[",1)
+ 
+BamFileSortIndexVisualization2(re,genome)
+
+}
+
+BamFileSortIndexVisualization2 <- function(input,genome) {
   
-  #names(file.name.3)=sample.name
+  #library(ChIPpeakAnno)
   
-  #file.name.4 <-file.name.3[-1]
+  re<-input
   
-  #re.out<-lapply(file.name.4,function(u){
-  #  re=toGRanges(u,format="BED")
-    #colnames(re)=c("Count","GeneName")
-  #  re
-  #})
+  file.name.2<-re$input
+  output.dir.name=re$output
   
-  #head(re.out[[1]])
+  temp3=paste0(output.dir.name,"_visualization")
   
-#   re.out.L<-lapply(re.out,function(u){
-#     re=length(u)
-#     #colnames(re)=c("Count","GeneName")
-#     re
-#   })
-#   
-#   
-# #  cmd="samtools sort"
-# #  input.file="/scratch/projects/bbc/Project/Danny_chip2/Alignment/BWA/""2016-10-14-Hyunho-Yoon-16-1833-PF1502-1-p27_S4_R1.bam" 
-# #  output.file="/scratch/projects/bbc/aiminy_project/Bam_marked_sorted/""2016-10-14-Hyunho-Yoon-16-1833-PF1502-1-p27_S4_R1.bam_sorted"
-#   
-#   if(genome=="Mm"){
-#     
-#     annoData <- toGRanges(EnsDb.Mmusculus.v75, feature="gene")
-#     
-#     ol <- findOverlapsOfPeaks(re.out[c(2,4,1)])
-#     
-#     overlaps<-ol$peaklist$`11_2470IUPUI_WT_BM_SMC1_peaks.bed///13_2470IUPUI_WT_BM_Rad21_peaks.bed///10_WT_BM_ASXL1_peaks.bed`
-#     
-#     binOverFeature(overlaps, annotationData=annoData,
-#                    radius=5000, nbins=20, FUN=length, errFun=0,
-#                    ylab="count",
-#                    main="Distribution of aggregated peak numbers around TSS")
-#     
-#     overlaps.trimmed<-trim(overlaps, use.names=TRUE)
-#     
-#     library(EnsDb.Mmusculus.v79)
-#     dd.GRCm39.mm10<-toGRanges(EnsDb.Mmusculus.v75)
-#     #seqinfo(dd.GRCm39.mm10)
-#     #seqlevels(dd.GRCm39.mm10)
-#     
-#     seqlevels(dd.GRCm39.mm10,force=TRUE) <- c("chr1","chr10","chr11","chr12","chr13",
-#                                               "chr14","chr15","chr16","chr17","chr18","chr19","chr2",
-#                                               "chr3","chr4","chr5","chr6","chr7","chr8","chr9","chrX","chrY")
-#     
-#     seqinfo(overlaps)<-seqinfo(dd.GRCm39.mm10)
-#     #GRCm38/mm10
-#     #dd<-toGRanges(EnsDb.Mmusculus.v79)
-#     #seqinfo(dd)
-#     #library(ensembldb)
-#     #library(GenomeInfoDb)
-#     seqlevelsStyle(overlaps.trimmed) <- seqlevelsStyle(dd.GRCm39.mm10)
-#     overlaps.anno<-annoPeaks(overlaps.trimmed,dd.GRCm39.mm10)
-#     write.table(overlaps.anno,file=paste0(temp3,"/","annotation.txt"),row.names = FALSE,quote=FALSE,sep="\t")
-#   }else if(genome=="Hs"){
-#     
-#     library(EnsDb.Hsapiens.v75)
-#     #annoData<-toGRanges(EnsDb.Hsapiens.v75, feature="gene")
-#     
-#     dd.hs<-toGRanges(EnsDb.Hsapiens.v75)
-#     
-#     print(seqinfo(dd.hs))
-#     print(seqlevels(dd.hs))
-#     
-#     #print(seqlevels(dd.hs)[,1])
-#     
-#     #print(seqlevels(re.out[[1]])[,1])
-#     
-#     # seqlevels(dd.hs,force=TRUE) <- c("chr1","chr10","chr11","chr12","chr13",
-#     #                                           "chr14","chr15","chr16","chr17","chr18","chr19","chr2",
-#     #                                           "chr3","chr4","chr5","chr6","chr7","chr8","chr9","chrX","chrY")
-#     
-#     #temp4=
-#     
-#     re.out.L<-lapply(1:length(re.out),function(u,re.out,dd.hs){
-#       
-#       x=re.out[[u]]
-#       x_name=names(re.out)[u]
-#       
-#       seqlevels(dd.hs,force=TRUE)<-seqinfo(x)@seqnames
-#       #print(seqinfo(re.out.trimmed))
-#       #print(seqlevels(re.out.trimmed))
-#       seqinfo(x)<-seqinfo(dd.hs)
-#       #GRCm38/mm10
-#       #dd<-toGRanges(EnsDb.Mmusculus.v79)
-#       #seqinfo(dd)
-#       #library(ensembldb)
-#       #library(GenomeInfoDb)
-#       seqlevelsStyle(x) <- seqlevelsStyle(dd.hs)
-#       re.out.trimmed<-trim(x, use.names=TRUE)
-#       overlaps.anno<-annoPeaks(re.out.trimmed,dd.hs)
-#       
-#       write.table(overlaps.anno,file=paste0(temp3,"/",x_name,"_annotation.txt"),row.names = FALSE,quote=FALSE,sep="\t")
-#     },re.out,dd.hs)
-#     
-#   }
+  dir.create(temp3)
+  
+  re.out<-file.name.2
+  
+  cmd1="samtools sort"
+  
+  lapply(1:length(re.out),function(u,re.out,temp3){
+    
+    x=re.out[[u]]
+    x_name=names(re.out)[u]
+    cmd2=paste0(cmd1," ",x," ",paste0(temp3,"/",x_name,"_sorted"))
+    
+    #print(cmd2)
+    system(cmd2)
+    
+  },re.out,temp3)
+  
+  cmd3="samtools index"
+  
+  lapply(1:length(re.out),function(u,re.out,temp3){
+    
+    x=re.out[[u]]
+    x_name=names(re.out)[u]
+    cmd4=paste0(cmd3," ",paste0(temp3,"/",x_name,"_sorted.bam"))
+    
+    #print(cmd2)
+    system(cmd4)
+    
+  },re.out,temp3)
+  
+  cmd5="ngs.plot.r -G hg19 -R tss -C"
+  cmd6="-O"
+  cmd7="-L 4000"
+  #cmd3="-L 4000 -RR 1 -CD 1 -CO \\\"blue\\\""
+  
+  #ngs.plot.r -G hg19 -R tss -C $1 -O $2 -L 4000 -RR 1 -CD 1 -CO "blue"
+  
+  #file.name.3<-file.name.2[-6]
+  
+  lapply(1:length(re.out),function(u,re.out,temp3){
+    
+    x=re.out[[u]]
+    x_name=names(re.out)[u]
+    
+    cmd8=paste(cmd5,paste0(temp3,"/",paste0(x_name,"_sorted.bam")),cmd6,paste0(temp3,"/",paste0(x_name,"_sorted")),cmd7,sep=" ")
+    
+    print(cmd8)
+    system(cmd8, intern = TRUE, ignore.stderr = TRUE)
+    
+    #re=read.table(u,header=FALSE)
+    #  re<-as.character(re[,1])
+    #  #colnames(re)=c("Count","GeneName")
+    #  re
+  },re.out,temp3)
   
 }
+
+
+
