c9af4eb8150b6d463f57299c75fd6943f15bf5f0
aiminy <jlfmssm@gmail.com>
2017-03-15 12:10:41 -0400

    add check OS function

--- R/utility.R
+++ R/utility.R
@@ -56,3 +56,19 @@ list_to_df <- function(list_for_df) {
   df$value <- unname(list_for_df)
   df
 }
+
+get_os <- function(){
+  sysinf <- Sys.info()
+  if (!is.null(sysinf)){
+    os <- sysinf['sysname']
+    if (os == 'Darwin')
+      os <- "osx"
+  } else { ## mystery machine
+    os <- .Platform$OS.type
+    if (grepl("^darwin", R.version$os))
+      os <- "osx"
+    if (grepl("linux-gnu", R.version$os))
+      os <- "linux"
+  }
+  tolower(os)
+}
