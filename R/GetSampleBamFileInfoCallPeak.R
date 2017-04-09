9636cc16c34384b4c1ba611573ed67ee3aeb7683
aiminy <jlfmssm@gmail.com>
2017-04-09 18:44:59 -0400

    remove unused code

--- R/GetSampleBamFileInfoCallPeak.R
+++ R/GetSampleBamFileInfoCallPeak.R
@@ -18,27 +18,25 @@
 #' re <- GetSampleInfo(input.sample.file,input.bam.file)
 #' 
 #' 
-GetSampleInfo <- function(input.sample.file, input.bam.file) {
+GetSampleInfo <- function(input.sample.file, input.bam.file)
+{
     
     re <- read.csv(input.sample.file)
     re.c <- colnames(re)
     
     bam.file <- read.table(input.bam.file, header = FALSE)
     
-    # print(bam.file)
-    
-    yy <- apply(bam.file, 1, function(u) {
+    yy <- apply(bam.file, 1, function(u)
+    {
         y <- basename(u)
         pos <- regexpr("\\.", y)
         pos <- pos - 1
-        # cat(pos,'\n')
         y <- substr(y, 1, pos)
         y
     })
     
     bam.file.sample.name <- cbind(bam.file, yy)
     
-    # print(bam.file.sample.name)
     colnames(bam.file.sample.name) <- c("file.name", "ID")
     
     Ab.type <- unique(as.character(re[, colnames(re) == re.c[3]]))
@@ -50,22 +48,24 @@ GetSampleInfo <- function(input.sample.file, input.bam.file) {
     
     colnames(re11)[4] = "Cell_TF"
     
-    chipseqCell <- function(n, es) {
+    chipseqCell <- function(n, es)
+    {
         value <- list(name = n, es = es)
         attr(value, "class") <- "ChipSeqCell"
         value
     }
     
-    cellType <- unique(as.character(re11[re11$Type_TF == "Input", ]$Type_Cell))
+    cellType <- unique(as.character(re11[re11$Type_TF == "Input", 
+        ]$Type_Cell))
     
     
-    yy <- lapply(cellType, function(u, re11) {
+    yy <- lapply(cellType, function(u, re11)
+    {
         n <- nchar(u)
-        # cat(u, n,'\n')
-        
         index1 <- which(substr(re11$Type_Cell, 1, n) == u)
         z <- re11[index1, ]
-        index2 <- which(nchar(as.character(z$Type_Cell)) <= n + 2)
+        index2 <- which(nchar(as.character(z$Type_Cell)) <= n + 
+            2)
         
         z2 <- z[index2, ]
         
@@ -74,15 +74,13 @@ GetSampleInfo <- function(input.sample.file, input.bam.file) {
         cc
     }, re11)
     
-    
-    # print(yy)
-    
-    
     colnames(re1)[4] = "Cell_TF"
     
     Cell.Ab.type <- unique(as.character(re1[, 4]))
     
-    re2 <- lapply(1:length(Cell.Ab.type), function(u, Cell.Ab.type, re1) {
+    re2 <- lapply(1:length(Cell.Ab.type), function(u, Cell.Ab.type, 
+        re1)
+        {
         
         x = Cell.Ab.type[u]
         z = re1
@@ -94,13 +92,12 @@ GetSampleInfo <- function(input.sample.file, input.bam.file) {
     
     names(re2) <- Cell.Ab.type
     
-    # Get the combination of cell type and antibody with replicates
-    
     re21 <- re2[lapply(re2, length) == 1]
     
     re22 <- re2[lapply(re2, length) == 2]
     
-    re3 <- list(re1 = re1, re2 = re2, re21 = re21, re22 = re22, re11 = re11, y = yy)
+    re3 <- list(re1 = re1, re2 = re2, re21 = re21, re22 = re22, 
+        re11 = re11, y = yy)
     
     return(re3)
     
@@ -115,8 +112,10 @@ GetSampleInfo <- function(input.sample.file, input.bam.file) {
 #' re <- peakcallwithinput(input.sample.file,input.bam.file,genome,output.dir,peakcaller)
 #' 
 #' 
-peakcallwithinput <- function(input.sample.file, input.bam.file, genome = c("Hs", 
-    "hs", "HS", "hS"), output.dir, peakcaller = c("macs14", "macs2"), peakPvalue) {
+peakcallwithinput <- function(input.sample.file, input.bam.file, 
+    genome = c("Hs", "hs", "HS", "hS"), output.dir, peakcaller = c("macs14", 
+        "macs2"), peakPvalue)
+        {
     
     re <- GetSampleInfo(input.sample.file, input.bam.file)
     
@@ -126,7 +125,8 @@ peakcallwithinput <- function(input.sample.file, input.bam.file, genome = c("Hs"
     
     temp3 = file.path(output.dir.name, output.dir)
     
-    if (!dir.exists(temp3)) {
+    if (!dir.exists(temp3))
+    {
         dir.create(temp3)
     }
     
@@ -137,7 +137,7 @@ peakcallwithinput <- function(input.sample.file, input.bam.file, genome = c("Hs"
     
     switch(peakcaller, macs2 = {
         
-        PATH1=Sys.getenv("PATH")
+        PATH1 = Sys.getenv("PATH")
         
         macs2_Lib = file.path("/nethome/axy148/NGS_tools/MACS/bin/")
         
@@ -148,38 +148,21 @@ peakcallwithinput <- function(input.sample.file, input.bam.file, genome = c("Hs"
         cat(cmd1, "\n")
         
         cmd9 = paste(cmd1, "callpeak -t", sep = " ")
-        # cmd10 = '-f BAM -g hs -n ' cmd11 = ' -m 6 18 --bw=200 -p 0.00001'
-        #cmd11 <- paste("-m 6 18 --bw=200", "-p", peakPvalue, sep = " ")
-        cmd11 <- paste("-p", peakPvalue, sep = " ")
-        
+  
         cmd11 <- paste("-p", peakPvalue, sep = " ")
         
     }, {
         cmd9 = "macs14 -t "
-        # cmd10 = '-f BAM -g hs -n ' cmd11 = ' -m 6,18 --bw=200 -p 0.00001'
-        
-        #cmd11 <- paste("-m 6,18 --bw=200", "-p", peakPvalue, sep = " ")
-        
+
         cmd11 <- paste("-p", peakPvalue, sep = " ")
         
     })
     
-    # #re.out<-file.name.2 '/nethome/axy148/MACS/bin/macs2 callpeak -t
-    # /scratch/projects/bbc/BAM2BW/'2016-03-23-2-18_S7_'_bam_sorted_by_position.bam
-    # -c
-    # /scratch/projects/bbc/Project/Lluis/Alignment/2016-03-20-1-1_S5__BWA/2016-03-20-1-1_S5_.bam
-    # -f BAM -g mm -n '2016-03-23-2-18_S7_'_bam_mm --outdir
-    # '/scratch/projects/bbc/Peak_chip_seq/Peak_chip_broad' -B -q 0.01' cmd9='macs14
-    # -t ' cmd10='-f BAM -g hs -n ' cmd11=' -m 6,18 --bw=200 -p 0.00001'
-    
-    cellInfo.run <- lapply(1:length(cellInfo), function(u, cellInfo, temp3) {
-        
-        # x=cellInfo[[u]]
+    cellInfo.run <- lapply(1:length(cellInfo), function(u, cellInfo, 
+        temp3)
+        {
         
         x.name = cellInfo[[u]]$name
-        
-        
-        # cat(x_name,'\n')
         
         es <- cellInfo[[u]]$es
         
@@ -187,7 +170,8 @@ peakcallwithinput <- function(input.sample.file, input.bam.file, genome = c("Hs"
         
         x.sample <- es[es$Type_TF != "Input", ]
         
-        x.run <- apply(x.sample, 1, function(x) {
+        x.run <- apply(x.sample, 1, function(x)
+        {
             
             y <- x
             
@@ -196,50 +180,36 @@ peakcallwithinput <- function(input.sample.file, input.bam.file, genome = c("Hs"
             Type_TF <- y[3]
             Cell_TF <- y[4]
             file.name <- y[5]
-            
-            
-            
             xx <- file.name
-            xx.name = paste(ID, gsub(" ", "-", Type_Cell), Type_TF, sep = "-")
+            xx.name = paste(ID, gsub(" ", "-", Type_Cell), Type_TF, 
+                sep = "-")
             
-            cmd12 = paste(cmd9, xx, "-c", x.input, cmd10, file.path(temp3, paste0(xx.name, 
-                "_hs_1.00e-05_", peakcaller)), cmd11, sep = " ")
-            # 
+            cmd12 = paste(cmd9, xx, "-c", x.input, cmd10, file.path(temp3, 
+                paste0(xx.name, "_hs_1.00e-05_", peakcaller)), 
+                cmd11, sep = " ")
+            
             cmd12
-            # print(cmd12)
+            
         })
-        
-        # cat(x.name,'\n')
-        
-        # print(x.run)
         
         x.run
         
-        # cmd12=paste(cmd9,x,'–c',x.input,cmd10,paste0(temp3,'/',paste0(x_name,'_hs_1.00e-05_macs142')),cmd11,sep='
-        # ')
-        
-        # print(x.input)
-        
-        # system(cmd12, intern = TRUE, ignore.stderr = TRUE)
-        
-        # re=read.table(u,header=FALSE) re<-as.character(re[,1])
-        # #colnames(re)=c('Count','GeneName') re
     }, cellInfo, temp3)
     
     
-    names(cellInfo.run) = unlist(lapply(cellInfo, function(u) {
+    names(cellInfo.run) = unlist(lapply(cellInfo, function(u)
+    {
         u$name
     }))
     
     zzz <- unlist(cellInfo.run)
     
-    lapply(1:length(zzz), function(u, zzz) {
+    lapply(1:length(zzz), function(u, zzz)
+    {
         
         cat(as.character(zzz[u][[1]]), "\n")
         cat("\n")
-        
-        # system(as.character(zzz[u][[1]]), intern = TRUE, ignore.stderr = TRUE)
-        
+      
         system(as.character(zzz[u][[1]]))
         
     }, zzz)
@@ -247,59 +217,54 @@ peakcallwithinput <- function(input.sample.file, input.bam.file, genome = c("Hs"
     
     re <- list(cellInforun = cellInfo.run, zzz = zzz)
     
-    # AnntationUsingChipSeeker(temp3,)
-    # dirname('/Volumes/Bioinformatics$/2017/DannyNewData/PeakCall/')
-    
     AnntationUsingChipSeeker(temp3, "peaks.bed", temp3, DD = 5000)
     
     return(re)
     
-    # AnnotatePeak2(paste0(temp3,'/'),'*macs142_peaks.bed',7,paste0(output.dir.name,'PeakAnnotation_at_',temp2),genome='Hs')
-    
-    # AnnotatePeak3(paste0(temp3,'/'),paste0(output.dir.name,'_PeakAnnotation'),
-    # genome='Hs')
-    
-    # BamFileSortIndexVisualization2(re,genome)
-    
 }
 
-select.sample <- c("MDA MB 231-DD-1_cJun","MDA MB 231-1_cJun","1833-1_cJun")
+select.sample <- c("MDA MB 231-DD-1_cJun", "MDA MB 231-1_cJun", 
+    "1833-1_cJun")
 output.config.dir <- "~/"
 
-configAndMultiplot <- function(res,select.sample,output.config.dir){
-  
-  #res <- GetSampleInfo(input.sample.file,input.bam.file)
-  
-  if(!dir.exists(output.config.dir)){dir.create(output.config.dir)}
-  
-  x <- res$re11
-  
-  xx <- x[which(x$Cell_TF %in% select.sample),]
-  
-  xxx <- cbind.data.frame(xx$file.name,rep("-1",dim(xx)[1]),gsub(" ","-",xx$Cell_TF))
-  
-  config.sample.name <- paste(gsub(" ","-",select.sample),collapse = "-and-")
+configAndMultiplot <- function(res, select.sample, output.config.dir)
+{
     
-  config.file.name <- file.path(output.config.dir,paste0(config.sample.name,"-config.txt"))
-  
-  write.table(xxx,file=config.file.name,col.names = FALSE,
-              row.names = FALSE,quote=FALSE,sep="\t")
-  
-  cmd0 <- "ngs.plot.r -G hg19 -R tss -C" 
-  
-  cmd1 <- "-O" 
-  
-  cmd2 <- "-L 4000 -RR 1 -CD 1 -CO \"blue\""
-
-  output.results <- file.path(output.config.dir,paste0(config.sample.name,"results"))
+    if (!dir.exists(output.config.dir))
+    {
+        dir.create(output.config.dir)
+    }
     
-  cmd <- paste(cmd0,config.file.name,cmd1,output.results,cmd2,sep=" ")  
+    x <- res$re11
     
-  system(cmd)
-  
-  return(config.file.name)
-  
+    xx <- x[which(x$Cell_TF %in% select.sample), ]
+    
+    xxx <- cbind.data.frame(xx$file.name, rep("-1", dim(xx)[1]), 
+        gsub(" ", "-", xx$Cell_TF))
+    
+    config.sample.name <- paste(gsub(" ", "-", select.sample), 
+        collapse = "-and-")
+    
+    config.file.name <- file.path(output.config.dir, paste0(config.sample.name, 
+        "-config.txt"))
+    
+    write.table(xxx, file = config.file.name, col.names = FALSE, 
+        row.names = FALSE, quote = FALSE, sep = "\t")
+    
+    cmd0 <- "ngs.plot.r -G hg19 -R tss -C"
+    
+    cmd1 <- "-O"
+    
+    cmd2 <- "-L 4000 -RR 1 -CD 1 -CO \"blue\""
+    
+    output.results <- file.path(output.config.dir, paste0(config.sample.name, 
+        "results"))
+    
+    cmd <- paste(cmd0, config.file.name, cmd1, output.results, 
+        cmd2, sep = " ")
+    
+    system(cmd)
+    
+    return(config.file.name)
+    
 }
-
-
-
--- inst/bin/Run_Chip_Seq_interactive_model.r
+++ inst/bin/Run_Chip_Seq_interactive_model.r
@@ -8,13 +8,16 @@
 
 R_lib = .libPaths()[1]
 
-get_os <- function() {
+get_os <- function()
+{
     sysinf <- Sys.info()
-    if (!is.null(sysinf)) {
+    if (!is.null(sysinf))
+    {
         os <- sysinf["sysname"]
         if (os == "Darwin") 
             os <- "osx"
-    } else {
+    } else
+    {
         ## mystery machine
         os <- .Platform$OS.type
         if (grepl("^darwin", R.version$os)) 
@@ -35,57 +38,62 @@ cat("\tAvaliable analysis: \t1. peakcalling \t2. annotation \t3. visualization \
 input <- file("stdin", "r")
 row <- readLines(input, n = 1)
 
-if(row == 1){
-  choose.type <- "peakcalling"
+if (row == 1)
+{
+    choose.type <- "peakcalling"
 }
 
-if(row == 2){
-  choose.type <- "annotation"
+if (row == 2)
+{
+    choose.type <- "annotation"
 }
 
-if(row == 3){
-  choose.type <- "visualization"
+if (row == 3)
+{
+    choose.type <- "visualization"
 }
 
-if(row == 4){
-  choose.type <- "All"
+if (row == 4)
+{
+    choose.type <- "All"
 }
 
-analysisVisualization <- function(R_lib) {
-  
-  cat("You choose to visualize the sorted bam files, please define the following setting parameters: \n",
-      "input.sample.info.file (ex: /scratch/projects/bbc/aiminy_project/DannyNewData2/SampleID_INFO_ChIP_new_Danny.csv)\n",
-      "input.bam.list.file (ex: /scratch/projects/bbc/aiminy_project/DannyNewData2/sorted_bam_files.txt)\n",
-      "output.figure.dir (ex:Visualization)\n")
-  
-  input <- file("stdin", "r");
-  count.file.dir <- readLines(input, n = 3);
-  
-  input.sample.file <- count.file.dir[1]
-  input.bam.file <- count.file.dir[2]
-  output.config.dir <- count.file.dir[3]
-  
-  library(ChipSeq)
-  res <- GetSampleInfo(input.sample.file,input.bam.file);
-  print(res$re11);
-  cat("please choose samples from Cell_TF :\n");
-  
-  input <- file("stdin", "r");
-  samples.choosed <- readLines(input, n = 1);
-  
-  #system(paste0(cmd2, " ", cmd3))
-  
-  #print(count.file.dir)
-  
+analysisVisualization <- function(R_lib)
+{
+    
+    cat("You choose to visualize the sorted bam files, please define the following setting parameters: \n", 
+        "input.sample.info.file (ex: /scratch/projects/bbc/aiminy_project/DannyNewData2/SampleID_INFO_ChIP_new_Danny.csv)\n", 
+        "input.bam.list.file (ex: /scratch/projects/bbc/aiminy_project/DannyNewData2/sorted_bam_files.txt)\n", 
+        "output.figure.dir (ex:Visualization)\n")
+    
+    input <- file("stdin", "r")
+    count.file.dir <- readLines(input, n = 3)
+    
+    input.sample.file <- count.file.dir[1]
+    input.bam.file <- count.file.dir[2]
+    output.config.dir <- count.file.dir[3]
+    
+    library(ChipSeq)
+    res <- GetSampleInfo(input.sample.file, input.bam.file)
+    print(res$re11)
+    cat("please choose samples from Cell_TF :\n")
+    
+    input <- file("stdin", "r")
+    samples.choosed <- readLines(input, n = 1)
+    
+    # system(paste0(cmd2, ' ', cmd3))
+    
+    # print(count.file.dir)
+    
     cmd1 = "bsub -P bbc -J \"VBam\" -o %J.VBam.log -e %J.VBam.err -W 72:00 -n 8 -q general -u aimin.yan@med.miami.edu"
     
     cmd2 = paste0(R_lib, "/ChipSeq/bin/Visualization.r")
     
-    tmp <- paste(count.file.dir,collapse = " ")
+    tmp <- paste(count.file.dir, collapse = " ")
     
-    tmp2 <- paste(tmp,samples.choosed,sep=" ")
-      
-    cmd3 = paste("Rscript",cmd2,tmp2)
+    tmp2 <- paste(tmp, samples.choosed, sep = " ")
+    
+    cmd3 = paste("Rscript", cmd2, tmp2)
     
     print(cmd3)
     
@@ -95,199 +103,203 @@ analysisVisualization <- function(R_lib) {
     
 }
 
-analysisAll <- function(R_lib) {
-  cat("Do you want to perform peak calling, annotation, and coverage visualization?\n")
-  
-  input <- file("stdin", "r")
-  row <- readLines(input, n = 1)
-  
-  print(row)
-  
-  if (row == "Yes") {
-    
-    
-    
-    cat("Do you have input bam file for control ?\n")
+analysisAll <- function(R_lib)
+{
+    cat("Do you want to perform peak calling, annotation, and coverage visualization?\n")
     
     input <- file("stdin", "r")
     row <- readLines(input, n = 1)
     
-    if (row == "No") {
-      
-      cat("please define the input Bam file directory:\n")
-      
-      input <- file("stdin", "r")
-      input.file.dir <- readLines(input, n = 1)
-      
-      # input.file.dir=row
-      
-      cat("please define the output file directory:\n")
-      
-      input <- file("stdin", "r")
-      output.file.dir <- readLines(input, n = 1)
-      
-      # out.file.dir=row
-      
-      cat("please define genome name:\n")
-      
-      input <- file("stdin", "r")
-      genome <- readLines(input, n = 1)
-      
-      cmd1 = "bsub -P bbc -J \"RunR\" -o %J.RunR.log -e %J.RunR.err -W 72:00 -n 8 -q general -u aimin.yan@med.miami.edu"
-      cmd2 = paste0("Rscript ", R_lib, "/ChipSeq/bin/Run_Chip_Seq.r ", 
-                    input.file.dir, " ", output.file.dir, " ", genome)
-      
-      system(paste0(cmd1, " ", cmd2))
-      
-      # cmd2=paste0()
-      
-      # system(cmd2)
-      
-      cat("Finished peak calling, annotation, and coverage visualizatio...\n")
-      
-    } else if (row == "Yes") {
-      
-      cat("please define the sample information file:\n")
-      
-      input <- file("stdin", "r")
-      sample.info.file <- readLines(input, n = 1)
-      
-      # input.file.dir=row
-      
-      cat("please define the Bam files information file:\n")
-      
-      input <- file("stdin", "r")
-      bam.info.file <- readLines(input, n = 1)
-      
-      # out.file.dir=row
-      
-      cat("please define genome name:\n")
-      
-      input <- file("stdin", "r")
-      genome <- readLines(input, n = 1)
-      
-      cat("Which peakcaller you want to use, please choose: macs14 or macs2 \n")
-      
-      input <- file("stdin", "r")
-      peakcaller <- readLines(input, n = 1)
-      
-      cat("The peakcaller you choose is :", peakcaller, "please define p value threshold for peak calling: \n")
-      
-      input <- file("stdin", "r")
-      peakPvalue <- readLines(input, n = 1)
-      
-      cat("Please define the name of output directory for peaks: \n")
-      
-      input <- file("stdin", "r")
-      peak.out.dir <- readLines(input, n = 1)
-      
-      peak.out.dir <- paste0(peak.out.dir,"_",genome,"_",peakcaller,"_",peakPvalue)
-      
-      cmd1 = "bsub -P bbc -J \"InputCall\" -o %J.InputCall.log -e %J.InputCall.err -W 72:00 -n 8 -q general -u aimin.yan@med.miami.edu"
-      cmd2 = paste("Rscript",file.path(R_lib,"ChipSeq/bin/UseInputCall.r"),
-                   sample.info.file,bam.info.file,genome,
-                   peak.out.dir,peakcaller,peakPvalue,sep=" ")
-      
-      system(paste0(cmd1, " ", cmd2))
-      
+    print(row)
+    
+    if (row == "Yes")
+    {
+        
+        
+        
+        cat("Do you have input bam file for control ?\n")
+        
+        input <- file("stdin", "r")
+        row <- readLines(input, n = 1)
+        
+        if (row == "No")
+        {
+            
+            cat("please define the input Bam file directory:\n")
+            
+            input <- file("stdin", "r")
+            input.file.dir <- readLines(input, n = 1)
+            
+            # input.file.dir=row
+            
+            cat("please define the output file directory:\n")
+            
+            input <- file("stdin", "r")
+            output.file.dir <- readLines(input, n = 1)
+            
+            # out.file.dir=row
+            
+            cat("please define genome name:\n")
+            
+            input <- file("stdin", "r")
+            genome <- readLines(input, n = 1)
+            
+            cmd1 = "bsub -P bbc -J \"RunR\" -o %J.RunR.log -e %J.RunR.err -W 72:00 -n 8 -q general -u aimin.yan@med.miami.edu"
+            cmd2 = paste0("Rscript ", R_lib, "/ChipSeq/bin/Run_Chip_Seq.r ", 
+                input.file.dir, " ", output.file.dir, " ", genome)
+            
+            system(paste0(cmd1, " ", cmd2))
+            
+            # cmd2=paste0()
+            
+            # system(cmd2)
+            
+            cat("Finished peak calling, annotation, and coverage visualizatio...\n")
+            
+        } else if (row == "Yes")
+        {
+            
+            cat("please define the sample information file:\n")
+            
+            input <- file("stdin", "r")
+            sample.info.file <- readLines(input, n = 1)
+            
+            # input.file.dir=row
+            
+            cat("please define the Bam files information file:\n")
+            
+            input <- file("stdin", "r")
+            bam.info.file <- readLines(input, n = 1)
+            
+            # out.file.dir=row
+            
+            cat("please define genome name:\n")
+            
+            input <- file("stdin", "r")
+            genome <- readLines(input, n = 1)
+            
+            cat("Which peakcaller you want to use, please choose: macs14 or macs2 \n")
+            
+            input <- file("stdin", "r")
+            peakcaller <- readLines(input, n = 1)
+            
+            cat("The peakcaller you choose is :", peakcaller, 
+                "please define p value threshold for peak calling: \n")
+            
+            input <- file("stdin", "r")
+            peakPvalue <- readLines(input, n = 1)
+            
+            cat("Please define the name of output directory for peaks: \n")
+            
+            input <- file("stdin", "r")
+            peak.out.dir <- readLines(input, n = 1)
+            
+            peak.out.dir <- paste0(peak.out.dir, "_", genome, 
+                "_", peakcaller, "_", peakPvalue)
+            
+            cmd1 = "bsub -P bbc -J \"InputCall\" -o %J.InputCall.log -e %J.InputCall.err -W 72:00 -n 8 -q general -u aimin.yan@med.miami.edu"
+            cmd2 = paste("Rscript", file.path(R_lib, "ChipSeq/bin/UseInputCall.r"), 
+                sample.info.file, bam.info.file, genome, peak.out.dir, 
+                peakcaller, peakPvalue, sep = " ")
+            
+            system(paste0(cmd1, " ", cmd2))
+            
+        }
+        
+    } else if (row == "No")
+    {
+        
+        cat("Do you want to perform peak merge, overlap, produing Venn diagram?\n")
+        
+        input <- file("stdin", "r")
+        row <- readLines(input, n = 1)
+        
+        if (row == "Yes")
+        {
+            
+            cat("please defne the input file for sample information:\n")
+            
+            input <- file("stdin", "r")
+            input.file.sample <- readLines(input, n = 1)
+            
+            cat("Finished to get sample information\n")
+            
+            
+            cat("please defne the input file directory for bed files:\n")
+            
+            input <- file("stdin", "r")
+            input.file.dir <- readLines(input, n = 1)
+            
+            cat("please defne the input file pattern:\n")
+            input <- file("stdin", "r")
+            input.file.pattern <- readLines(input, n = 1)
+            
+            cat("please defne the output file directory:\n")
+            input <- file("stdin", "r")
+            output.file.dir <- readLines(input, n = 1)
+            
+            R_lib = .libPaths()[1]
+            
+            cmd1 = "bsub -P bbc -J \"RunR\" -o %J.RunR.log -e %J.RunR.err -W 72:00 -n 8 -q general -u aimin.yan@med.miami.edu"
+            cmd2 = paste0("Rscript ", R_lib, "/ChipSeq/bin/Run_Merge_peak.r ", 
+                input.file.dir, " ", output.file.dir, " ", input.file.pattern, 
+                " ", input.file.sample)
+            
+            system(paste0(cmd1, " ", cmd2))
+            
+            cat("Finished peak merge, overlap, produing Venn diagram?\n")
+        } else if (row == "No")
+        {
+            
+            cat("Do you want to perform  Bam files sorting, index and Visualization?\n")
+            
+            input <- file("stdin", "r")
+            row <- readLines(input, n = 1)
+            
+            if (row == "Yes")
+            {
+                
+                cat("please defne the input file directory for Bam files:\n")
+                
+                input <- file("stdin", "r")
+                input.file.dir <- readLines(input, n = 1)
+                
+                cat("Finished to get bam files\n")
+                
+                cat("please defne the out file directory:\n")
+                
+                input <- file("stdin", "r")
+                output.file.dir <- readLines(input, n = 1)
+                
+                cat("please defne genome:(example: Hs) \n")
+                
+                
+                input <- file("stdin", "r")
+                genomeID <- readLines(input, n = 1)
+                
+                cmd1 = "bsub -P bbc -J \"BamR\" -o %J.BamR.log -e %J.BamR.err -W 72:00 -n 8 -q general -u aimin.yan@med.miami.edu"
+                
+                cmd2 = paste0("Rscript ", R_lib, "/ChipSeq/bin/Process_Bam.r ", 
+                  input.file.dir, " ", output.file.dir, " ", 
+                  genomeID)
+                
+                system(paste0(cmd1, " ", cmd2))
+                
+                cat("Finished Bam files sorting, index and Visualization ?\n")
+                
+            } else
+            {
+                quit()
+            }
+        }
     }
-    
-  } else if (row == "No") {
-    
-    cat("Do you want to perform peak merge, overlap, produing Venn diagram?\n")
-    
-    input <- file("stdin", "r")
-    row <- readLines(input, n = 1)
-    
-    if (row == "Yes") {
-      
-      cat("please defne the input file for sample information:\n")
-      
-      input <- file("stdin", "r")
-      input.file.sample <- readLines(input, n = 1)
-      
-      cat("Finished to get sample information\n")
-      
-      
-      cat("please defne the input file directory for bed files:\n")
-      
-      input <- file("stdin", "r")
-      input.file.dir <- readLines(input, n = 1)
-      
-      cat("please defne the input file pattern:\n")
-      input <- file("stdin", "r")
-      input.file.pattern <- readLines(input, n = 1)
-      
-      cat("please defne the output file directory:\n")
-      input <- file("stdin", "r")
-      output.file.dir <- readLines(input, n = 1)
-      
-      R_lib = .libPaths()[1]
-      
-      cmd1 = "bsub -P bbc -J \"RunR\" -o %J.RunR.log -e %J.RunR.err -W 72:00 -n 8 -q general -u aimin.yan@med.miami.edu"
-      cmd2 = paste0("Rscript ", R_lib, "/ChipSeq/bin/Run_Merge_peak.r ", 
-                    input.file.dir, " ", output.file.dir, " ", input.file.pattern, 
-                    " ", input.file.sample)
-      
-      system(paste0(cmd1, " ", cmd2))
-      
-      cat("Finished peak merge, overlap, produing Venn diagram?\n")
-    } else if (row == "No") {
-      
-      cat("Do you want to perform  Bam files sorting, index and Visualization?\n")
-      
-      input <- file("stdin", "r")
-      row <- readLines(input, n = 1)
-      
-      if (row == "Yes") {
-        
-        cat("please defne the input file directory for Bam files:\n")
-        
-        input <- file("stdin", "r")
-        input.file.dir <- readLines(input, n = 1)
-        
-        cat("Finished to get bam files\n")
-        
-        cat("please defne the out file directory:\n")
-        
-        input <- file("stdin", "r")
-        output.file.dir <- readLines(input, n = 1)
-        
-        cat("please defne genome:(example: Hs) \n")
-        
-        
-        input <- file("stdin", "r")
-        genomeID <- readLines(input, n = 1)
-        
-        cmd1 = "bsub -P bbc -J \"BamR\" -o %J.BamR.log -e %J.BamR.err -W 72:00 -n 8 -q general -u aimin.yan@med.miami.edu"
-        
-        cmd2 = paste0("Rscript ", R_lib, "/ChipSeq/bin/Process_Bam.r ", 
-                      input.file.dir, " ", output.file.dir, " ", genomeID)
-        
-        system(paste0(cmd1, " ", cmd2))
-        
-        cat("Finished Bam files sorting, index and Visualization ?\n")
-        
-      } else {
-        quit()
-      }
-    }
-  }
 }
 
-switch (choose.type,
-        All = {
-          analysisAll(R_lib)
-        },
-        peakcalling= {
-        },
-        annotation={
-          
-        },
-        visualization={
-          analysisVisualization(R_lib)
-        }
-)
-
-
-
+switch(choose.type, All = {
+    analysisAll(R_lib)
+}, peakcalling = {
+}, annotation = {
+    
+}, visualization = {
+    analysisVisualization(R_lib)
+})
