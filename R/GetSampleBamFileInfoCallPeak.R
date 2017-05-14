49849d609d8a358d8f53b9495662c1d3008c313f
aiminy <jlfmssm@gmail.com>
2017-05-14 18:01:19 -0400

    test

--- R/GetSampleBamFileInfoCallPeak.R
+++ R/GetSampleBamFileInfoCallPeak.R
@@ -427,6 +427,9 @@ useBamCompare <- function(input.sample.file,input.bam.file,output.dir)
     
     x.sample <- es[es$Type_TF != "Input", ]
     
+    print(x.sample)
+    print(x.input)
+    
     x.run <- apply(x.sample, 1, function(x, x.input, temp3)
     {
       
@@ -447,7 +450,7 @@ useBamCompare <- function(input.sample.file,input.bam.file,output.dir)
     
       cmd4 <- paste(cmd1,cmd2,cmd3,sep=" ") 
         
-      cmd5 <- file.path(output.dir,paste0("log2ratio_",basename(xx),"_vs_",basename(x.input),".bw"))
+      cmd5 <- file.path(output.dir,paste0("log2ratio_",basename(as.character(xx)),"_vs_",basename(as.character(x.input)),".bw"))
       
       
       cmd6 <- paste(cmd4,cmd5,sep=" ") 
