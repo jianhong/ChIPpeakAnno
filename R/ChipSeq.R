830deea54a77a24838ee401f8258813d2ac5b0d6
aiminy <jlfmssm@gmail.com>
2017-08-02 18:24:31 -0400

    add plot

--- R/ChipSeq.R
+++ R/ChipSeq.R
@@ -5764,7 +5764,7 @@ getEffectOfRmDup<-function(){
 
 #' sbatch -P bbc -J "zhaoCJun" -o %J.zhaoCJun.log -e %J.zhaoCJun.err -W 72:00 -n 32 -q parallel -R 'rusage[mem= 16000 ] span[ptile= 16 ]' -u aimin.yan@med.miami.edu R -e 'library(ChipSeq);re <- ChipSeq:::runIDR()'
 
-#'
+#' sbatch -p hipri test-job.sh
 #' sbatch -p hipri test-job.sh
 
 
