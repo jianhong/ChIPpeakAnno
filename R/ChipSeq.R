cbb62285414a02cc3a9eca673b162d0f9ec14001
aiminy <jlfmssm@gmail.com>
2017-09-27 18:38:27 -0400

    getSubmitSequence

--- R/ChipSeq.R
+++ R/ChipSeq.R
@@ -6171,3 +6171,92 @@ getBwUseBamCoverage2 <- function(input.infor.file,bl_file,output.dir)
   
 }
 
+getSummitSequence<-function(dir.name,input.file.pattern,genome,out.dir.name){
+  
+  if (!dir.exists(out.dir.name))
+  {
+    dir.create(out.dir.name, recursive = TRUE)
+  }
+  
+  
+  dir.name=reformatPath(dir.name)
+  out.dir.name=reformatPath(out.dir.name)
+  
+  file.name=paste0(dir.name,dir(dir.name,recursive = TRUE,pattern=input.file.pattern))
+  file.name.2<-as.list(file.name)
+  
+  names(file.name.2)=basename(file.name.2)
+  
+  print(file.name.2)
+  
+  re.out<-lapply(names(file.name.2),function(u){
+    
+    
+    #print(names(u))
+    uu=file.name.2[[u]]
+    
+    re=read.table(uu,header=TRUE)
+    colnames(re)[c(7,9)]=c("-10*LOG10(pvalue)","FDR(%)")
+    temp<-re
+    temp$start<-temp$start-1
+    temp$end<-temp$end-1
+    temp$summit<-temp$summit-1
+    temp
+    
+    summitPeak<-temp$start+temp$summit
+    
+    temp2<-temp
+    
+    temp2$start<-summitPeak-49
+    temp2$end<-summitPeak+50
+    temp2$summit<-summitPeak
+    temp2$length<-temp2$end-temp2$start+1
+    temp2
+    rownames(temp2)<-paste0("MACS_peak_",rownames(temp2))
+    temp3<-toGRanges(temp2)
+    names(temp3)<-rownames(temp2)
+    
+    
+    if(genome == "mm10")
+    {
+    dd.GRCm39.mm10<-toGRanges(EnsDb.Mmusculus.v75)
+    genome(temp3)<-genome(dd.GRCm39.mm10)
+    seqlevels(dd.GRCm39.mm10,force=TRUE) <- seqlevels(temp3)
+    seqinfo(temp3)<-seqinfo(dd.GRCm39.mm10)
+    temp3.trimmed<-trim(temp3, use.names=TRUE)
+    genome.mm10<-getBSgenome("BSgenome.Mmusculus.UCSC.mm10")
+    genome(temp3.trimmed)<-"mm10"
+    }
+    
+    if(genome == "hg19"){
+      
+      dd.GRCm39.mm10<-toGRanges(EnsDb.Hsapiens.v75)
+      genome(temp3)<-genome(dd.GRCm39.mm10)
+      seqlevels(dd.GRCm39.mm10,force=TRUE) <- seqlevels(temp3)
+      seqinfo(temp3)<-seqinfo(dd.GRCm39.mm10)
+      temp3.trimmed<-trim(temp3, use.names=TRUE)
+      genome.mm10<-getBSgenome("BSgenome.Hsapiens.UCSC.hg19")
+      genome(temp3.trimmed)<-"hg19"
+      
+    }
+    
+    
+    seq.temp3<-getAllPeakSequence(temp3.trimmed,genome=genome.mm10)
+    
+    write2FASTA(seq.temp3, paste0(out.dir.name,u,".fa"))
+    
+    re2<-list(originalPeak=re,down1Peak=temp,aroundSummit100Peak=temp2,GR=temp3)
+    re2
+  })
+  
+  sample.name<-sapply(strsplit(names(file.name.2),split="_peaks_"),"[[",1)
+  
+  names(re.out)<-sample.name
+  
+  
+  return(re.out)
+}
+
+
+
+
