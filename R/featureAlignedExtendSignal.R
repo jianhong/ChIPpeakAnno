b7bd99370987feae538206a92d7bf237cdd116e7
Jianhong Ou <jianhongou@Jianhongs-MBP.dhe.duke.edu>
2017-10-26 10:18:21 -0400

    make featureAlignedExtendSignal accept GAlignments list object

--- R/featureAlignedExtendSignal.R
+++ R/featureAlignedExtendSignal.R
@@ -6,7 +6,7 @@ featureAlignedExtendSignal <- function(bamfiles, index=bamfiles,
                                        librarySize,
                                        pe=c("auto", "PE", "SE"),
                                        adjustFragmentLength,
-                                       ...){
+                                       gal, ...){
     message("The signal is being calculated for DNA-seq.")
     if(missing(fragmentLength)){
         stop("fragmentLength is missing")
@@ -15,7 +15,25 @@ featureAlignedExtendSignal <- function(bamfiles, index=bamfiles,
       stopifnot(inherits(adjustFragmentLength, c("numeric", "integer")))
       stopifnot(length(adjustFragmentLength)==1)
     }
-    stopifnot(class(bamfiles)=="character")
+    if(!missing(bamfiles)){
+      stopifnot(class(bamfiles)=="character")
+      stopifnot(length(bamfiles)==length(index))
+      galInput <- FALSE
+    }else{
+      if(missing(gal)){
+        stop("gal is required if missing bamfiles")
+      }else{
+        if(!is(gal, "GAlignmentsList")){
+          galInput <- sapply(gal, function(.ele){
+            inherits(.ele, c("GAlignments", "GAlignmentPairs"))
+          })
+          if(any(!galInput)){
+            stop("gal must be a GAlignmentsList object or a list of GAlignmentPairs.")
+          }
+        }
+      }
+      galInput <- TRUE
+    }
     stopifnot(class(feature.gr)=="GRanges")
     if(missing(upstream) | missing(downstream)){
         stop("upstream and downstream is missing")
@@ -32,7 +50,6 @@ featureAlignedExtendSignal <- function(bamfiles, index=bamfiles,
     fragmentLength <- as.integer(fragmentLength)
     librarySize <- as.integer(librarySize)
     stopifnot(all(width(feature.gr)==1))
-    stopifnot(length(bamfiles)==length(index))
     
     totalBPinBin <- floor((upstream + downstream)/n.tile)# * length(feature.gr)
     feature.gr$oid <- 1:length(feature.gr)
@@ -55,40 +72,77 @@ featureAlignedExtendSignal <- function(bamfiles, index=bamfiles,
     grs <- unlist(grL)
     grs$gpid <- unlist(lapply(grL.eleLen, seq_len)) 
     grs$oid <- rep(feature.gr$oid, grL.eleLen)
-    
-    if(pe=="auto") {
-      pe <- mapply(function(file, id) 
-        suppressMessages(testPairedEndBam(file, index=id)), 
-                 bamfiles, index)
-    }else{
-      if(pe=="PE"){
-        pe <- TRUE
-      }else{
-        pe <- FALSE
-      }
-    }
-    param <- ScanBamParam(which=reduce(feature.gr.expand), 
-                          flag=scanBamFlag(isSecondaryAlignment=FALSE,
-                                          isNotPassingQualityControls=FALSE),
-                          what=scanBamWhat())
-    paramp <- ScanBamParam(which=reduce(feature.gr.expand), 
-                           flag=scanBamFlag(isProperPair=TRUE,
-                                            isSecondaryAlignment=FALSE,
-                                            isNotPassingQualityControls=FALSE),
-                           what=scanBamWhat())
-    bams.gr <- mapply(function(f, i, p, .fLen) {
-        if(!p){
-            .ele <- granges(readGAlignments(f, index=i, param=param))
+    if(galInput){
+      bams.gr <- mapply(function(ga, .fLen){
+        if(is(ga, "GAlignmentPairs")){
+          granges(.ele)
+        }else{
+          qname <- mcols(ga)$qname
+          if(pe=="auto"){
+            ## check qname
+            if(length(qname)==0){
+              pe <- FALSE
+            }else{
+              ## check duplicated qname
+              if(all(table(qname)==1)){
+                pe <- FALSE
+              }else{
+                if(all(table(qname)<3)){
+                  pe <- TRUE
+                }else{
+                  pe <- FALSE
+                }
+              }
+            }
+          }else{
+            pe <- pe=="PE"
+          }
+          
+          if(pe){
+            .ele <- split(ga, qname)
+            .ele <- granges(.ele, ignore.strand=TRUE)
+            .ele
+          }else{
+            .ele <- granges(ga)
             start(.ele[strand(.ele)=="-"]) <- 
-                end(.ele[strand(.ele)=="-"]) - .fLen + 1
+              end(.ele[strand(.ele)=="-"]) - .fLen + 1
             width(.ele) <- .fLen
             .ele
-        }else{
-            .ele <- readGAlignmentPairs(f, index=i, param=paramp)
-            .ele <- granges(.ele)
-            .ele
+          }
         }
-    }, bamfiles, index, pe, fragmentLength, SIMPLIFY=FALSE)
+      }, gal, fragmentLength, SIMPLIFY=FALSE)
+    }else{
+      if(pe=="auto") {
+        pe <- mapply(function(file, id) 
+          suppressMessages(testPairedEndBam(file, index=id)), 
+          bamfiles, index)
+      }else{
+        pe <- pe=="PE"
+      }
+      param <- ScanBamParam(which=reduce(feature.gr.expand), 
+                            flag=scanBamFlag(isSecondaryAlignment=FALSE,
+                                             isNotPassingQualityControls=FALSE),
+                            what=scanBamWhat())
+      paramp <- ScanBamParam(which=reduce(feature.gr.expand), 
+                             flag=scanBamFlag(isProperPair=TRUE,
+                                              isSecondaryAlignment=FALSE,
+                                              isNotPassingQualityControls=FALSE),
+                             what=scanBamWhat())
+      bams.gr <- mapply(function(f, i, p, .fLen) {
+        if(!p){
+          .ele <- granges(readGAlignments(f, index=i, param=param))
+          start(.ele[strand(.ele)=="-"]) <- 
+            end(.ele[strand(.ele)=="-"]) - .fLen + 1
+          width(.ele) <- .fLen
+          .ele
+        }else{
+          .ele <- readGAlignmentPairs(f, index=i, param=paramp)
+          .ele <- granges(.ele)
+          .ele
+        }
+      }, bamfiles, index, pe, fragmentLength, SIMPLIFY=FALSE)
+    }
+    
     if(!missing(adjustFragmentLength)){
       bams.gr <- lapply(bams.gr, reCenterPeaks, width=adjustFragmentLength)
       fragmentLength <- adjustFragmentLength
