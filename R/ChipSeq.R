25a44882956e313e906540b792a8d9dd2b20d193
Aimin Yan <aimin.at.work@gmail.com>
2018-08-16 15:58:08 -0400

    add AnnotatePeakUMASS function

--- R/ChipSeq.R
+++ R/ChipSeq.R
@@ -1,3 +1,129 @@
+#' AnnotatePeak
+#'
+#' @param input.file.dir 
+#' @param input.file.pattern 
+#' @param index.file 
+#' @param output.file.dir 
+#' @param genome 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' 
+#' input.file.dir='/Users/aiminyan/Aimin/Project/DropboxUmass/NADfinder/BedFiles'
+#' input.file.pattern="*.bed$"
+#' output.file.dir="/Users/aiminyan/Aimin/Project/DropboxUmass/NADfinder/Aimin/Output"
+#'
+#' AnnotatePeak(input.file.dir,input.file.pattern,8,output.file.dir,genome="Mm")
+#' AnnotatePeak(input.file.dir,input.file.pattern,7,output.file.dir,genome="Hs")
+#' 
+#' 
+#' 
+AnnotatePeakUMASS <- function(input.file.dir,input.file.pattern,output.file.dir,genome) {
+  
+  file.name.4 <- list.files(input.file.dir,pattern=input.file.pattern,all.files = TRUE,full.names = TRUE,recursive = TRUE,include.dirs = TRUE)
+  
+  re.out<-lapply(file.name.4,function(u){
+    peaks=read.table(u,skip=1)
+    colnames(peaks)[1:3]= c("chr","start","end")
+    peaks=toGRanges(peaks)
+    peaks
+  })
+  
+  names(re.out) <- gsub(" ","-",basename(file.name.4))
+  head(re.out[[1]])
+  
+  re.out.L<-lapply(re.out,function(u){
+    re=length(u)
+    #colnames(re)=c("Count","GeneName")
+    re
+  })
+  
+  if(genome=="Mm"){
+    
+    annoData <- toGRanges(EnsDb.Mmusculus.v75, feature="gene")
+    
+    #ol <- findOverlapsOfPeaks(re.out[[1]],re.out[[2]])
+    
+    #overlaps<-ol$peaklist$`11_2470IUPUI_WT_BM_SMC1_peaks.bed///13_2470IUPUI_WT_BM_Rad21_peaks.bed///10_WT_BM_ASXL1_peaks.bed`
+    
+    #binOverFeature(overlaps, annotationData=annoData,
+    #               radius=5000, nbins=20, FUN=length, errFun=0,
+    #               ylab="count",
+    #               main="Distribution of aggregated peak numbers around TSS")
+    
+    #overlaps.trimmed<-trim(overlaps, use.names=TRUE)
+    
+    #library(EnsDb.Mmusculus.v79)
+    dd.GRCm39.mm10<-toGRanges(EnsDb.Mmusculus.v75)
+    #seqinfo(dd.GRCm39.mm10)
+    #seqlevels(dd.GRCm39.mm10)
+    
+    # seqlevels(dd.GRCm39.mm10,force=TRUE) <- c("chr1","chr10","chr11","chr12","chr13",
+    #                                           "chr14","chr15","chr16","chr17","chr18","chr19","chr2",
+    #                                           "chr3","chr4","chr5","chr6","chr7","chr8","chr9","chrX","chrY")
+    
+    #seqinfo(overlaps)<-seqinfo(dd.GRCm39.mm10)
+    #GRCm38/mm10
+    #dd<-toGRanges(EnsDb.Mmusculus.v79)
+    #seqinfo(dd)
+    #library(ensembldb)
+    #library(GenomeInfoDb)
+    #seqlevelsStyle(overlaps.trimmed) <- seqlevelsStyle(dd.GRCm39.mm10)
+    
+    overlaps.trimmed<-trim(re.out[[1]],use.names=TRUE)
+    overlaps.anno<-annoPeaks(overlaps.trimmed,dd.GRCm39.mm10)
+    annotatedPeak1 <- annotatePeakInBatch(re.out[[1]], AnnotationData=annoData)
+    
+    if(!dir.exists(output.file.dir)){dir.create(output.file.dir,recursive = TRUE)}
+    write.table(overlaps.anno,file=file.path(output.file.dir,"annotation.txt"),row.names = FALSE,quote=FALSE,sep="\t")
+    write.table(annotatedPeak1,file=file.path(output.file.dir,"annotationInBatch.txt"),row.names = FALSE,quote=FALSE,sep="\t")
+  }else if(genome=="Hs"){
+    
+    library(EnsDb.Hsapiens.v75)
+    #annoData<-toGRanges(EnsDb.Hsapiens.v75, feature="gene")
+    
+    dd.hs<-toGRanges(EnsDb.Hsapiens.v75)
+    
+    print(seqinfo(dd.hs))
+    print(seqlevels(dd.hs))
+    
+    #print(seqlevels(dd.hs)[,1])
+    
+    #print(seqlevels(re.out[[1]])[,1])
+    
+    # seqlevels(dd.hs,force=TRUE) <- c("chr1","chr10","chr11","chr12","chr13",
+    #                                           "chr14","chr15","chr16","chr17","chr18","chr19","chr2",
+    #                                           "chr3","chr4","chr5","chr6","chr7","chr8","chr9","chrX","chrY")
+    
+    #temp4=
+    
+    re.out.L<-lapply(1:length(re.out),function(u,re.out,dd.hs){
+      
+      x=re.out[[u]]
+      x_name=names(re.out)[u]
+      
+      seqlevels(dd.hs,force=TRUE)<-seqinfo(x)@seqnames
+      #print(seqinfo(re.out.trimmed))
+      #print(seqlevels(re.out.trimmed))
+      seqinfo(x)<-seqinfo(dd.hs)
+      #GRCm38/mm10
+      #dd<-toGRanges(EnsDb.Mmusculus.v79)
+      #seqinfo(dd)
+      #library(ensembldb)
+      #library(GenomeInfoDb)
+      seqlevelsStyle(x) <- seqlevelsStyle(dd.hs)
+      re.out.trimmed<-trim(x, use.names=TRUE)
+      overlaps.anno<-annoPeaks(re.out.trimmed,dd.hs)
+      
+      write.table(overlaps.anno,file=paste0(temp3,"/",x_name,"_annotation.txt"),row.names = FALSE,quote=FALSE,sep="\t")
+    },re.out,dd.hs)
+    
+  }
+  
+}
+
 #' AnnotatePeak2
 #'
 #' @param input.file.dir 
