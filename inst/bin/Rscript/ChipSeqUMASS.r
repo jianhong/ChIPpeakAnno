8a068818a27e2f4c289b974ed1c47f3dd8a961b0
Aimin Yan <aimin.at.work@gmail.com>
2018-08-21 15:31:06 -0400

    load dplyr

--- inst/bin/Rscript/ChipSeqUMASS.r
+++ inst/bin/Rscript/ChipSeqUMASS.r
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 
 packages <- c("optparse","ChIPpeakAnno","ChIPseeker","EnsDb.Mmusculus.v75","DBI","tidyverse","annotate",
-              "reactome.db")
+              "reactome.db","dplyr")
 
 null<-lapply(packages, function(xxx) suppressMessages(require(xxx, character.only = TRUE,quietly=TRUE,warn.conflicts = FALSE)))
 
