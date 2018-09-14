192a1a9d3fbaea5f9413f9c4bb3f5f8817bee7df
Aimin Yan <aimin.at.work@gmail.com>
2018-09-14 15:00:28 -0400

    add binaryLogic

--- inst/bin/Rscript/ChipSeqUMASS.r
+++ inst/bin/Rscript/ChipSeqUMASS.r
@@ -1,7 +1,8 @@
 #!/usr/bin/env Rscript
 
 packages <- c("optparse","ChIPpeakAnno","ChIPseeker","EnsDb.Mmusculus.v75","DBI","tidyverse","annotate",
-              "reactome.db","dplyr","ggplot2","ggpubr","stringr","grid","Vennerable","devtools","reshape2")
+              "reactome.db","dplyr","ggplot2","ggpubr","stringr","grid","Vennerable","devtools","reshape2",
+              "binaryLogic")
 
 null<-lapply(packages, function(xxx) suppressMessages(require(xxx, character.only = TRUE,quietly=TRUE,warn.conflicts = FALSE)))
 
