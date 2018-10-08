f0822c98eb2876738bc703bd4802e15ee43e73ad
Aimin Yan <aimin.at.work@gmail.com>
2018-10-07 21:57:59 -0400

    change getGeneDensityPlot

--- R/ChipSeq.R
+++ R/ChipSeq.R
@@ -8447,44 +8447,111 @@ getNonCtcfBed <- function(re.out, BSgenome.Mmusculus.UCSC.mm10) {
   system(cmd)
 }
 
-getGeneDensityPlot <- function() {
+# bed.dir <- "/Users/aiminyan/Aimin/DropboxUmass/NADfinder/Aimin/Results_10_4_2018_gene_density"
+
+# bed.dir <- "/Users/aiminyan/Aimin/DropboxUmass/NADfinder/Aimin/Results_10_7_2018_gene_density"
+# getGeneDensityPlot(bed.dir)
+
+getGeneDensityPlot <- function(bed.dir) {
+  
+  if(!dir.exists(bed.dir)){dir.create(bed.dir,recursive = TRUE)}
   # whole genome in MB
   whole.genome.MB <- sum(width(gr.whole.genome))/1000000
   
-  export(re.out[[6]],file.path(bed.dir,"nonXL.bed"),format="BED")
-  export(re.out[[8]],file.path(bed.dir,"XL.bed"),format="BED")
+  names(XL.nonXL.subset) <- c("XL_Uniq","XL_ciLAD","XL_LAD","nonXL_Uniq","nonXL_ciLAD","nonXL_LAD")
   
-  nad.bed <- paste(file.path(bed.dir,"nonXL.bed"),file.path(bed.dir,"XL.bed"),sep= " ")
+  null <- lapply(1:length(XL.nonXL.subset), function(u,XL.nonXL.subset){
+    
+    x <- names(XL.nonXL.subset)[u] 
+    export(XL.nonXL.subset[[u]],file.path(bed.dir,paste0(x,".bed")),format="BED")
   
-  cmd = paste("bedops -u",nad.bed,">",file.path(bed.dir,"NAD.bed"))
+    },XL.nonXL.subset)
   
-  peaks=read.table(file.path(bed.dir,"NAD.bed"))
-  
-  colnames(peaks)[1:3]= c("chr","start","end")
-  NAD=toGRanges(peaks)
-  
-  NAD.before <- unique(c(re.out[[6]],re.out[[8]]))
-  
-  bed.dir <- "/Users/aiminyan/Aimin/DropboxUmass/NADfinder/Aimin/Results_10_4_2018_gene_density"
-  if(!dir.exists(bed.dir)){dir.create(bed.dir,recursive = TRUE)}
-  
-  export(NAD,file.path(bed.dir,"NAD.bed"),format="BED")
-  
-  
-  cmd = paste("bedops -d",ref.bed,file.path(bed.dir,"NAD.bed"),">",file.path(bed.dir,"nonNAD.bed"))
-  
+  xl.nad.bed <- file.path(bed.dir,paste0(c("XL_Uniq","XL_ciLAD","XL_LAD"),".bed"))
+  cmd = paste("bedops -u",paste(xl.nad.bed,collapse = " "),">",file.path(bed.dir,"XLNAD.bed"))
+  system(cmd)
+  cmd = paste("bedtools sort -i",file.path(bed.dir,"XLNAD.bed"),">",file.path(bed.dir,"XLNAD_sorted.bed"))
+  system(cmd) 
+  cmd = paste("bedtools merge -i",file.path(bed.dir,"XLNAD_sorted.bed"),">",file.path(bed.dir,"XLNAD_merged.bed"))
+  system(cmd) 
+  cmd = paste("bedops -d",ref.bed,file.path(bed.dir,"XLNAD_merged.bed"),">",file.path(bed.dir,"XL_nonNAD.bed"))
   system(cmd)
   
-  peaks=read.table(file.path(bed.dir,"nonNAD.bed"))
   
+  peaks=read.table(file.path(bed.dir,"XLNAD_merged.bed"))
   colnames(peaks)[1:3]= c("chr","start","end")
-  nonNAD=toGRanges(peaks)
+  XLNAD=toGRanges(peaks)
   
-  #mcols(NAD) <- NULL
-  #nonNAD <- setdiff(gr.whole.genome,NAD)
+  peaks=read.table(file.path(bed.dir,"XL_nonNAD.bed"))
+  colnames(peaks)[1:3]= c("chr","start","end")
+  XL_nonNAD=toGRanges(peaks)
   
-  NAD.MB <- sum(width(NAD))/1000000
-  nonNAD.MB <- sum(width(nonNAD))/1000000
+  
+  
+  nonxl.nad.bed <- file.path(bed.dir,paste0(c("nonXL_Uniq","nonXL_ciLAD","nonXL_LAD"),".bed"))
+  cmd = paste("bedops -u",paste(nonxl.nad.bed,collapse = " "),">",file.path(bed.dir,"nonXLNAD.bed"))
+  system(cmd)
+  cmd = paste("bedtools sort -i",file.path(bed.dir,"nonXLNAD.bed"),">",file.path(bed.dir,"nonXLNAD_sorted.bed"))
+  system(cmd) 
+  cmd = paste("bedtools merge -i",file.path(bed.dir,"nonXLNAD_sorted.bed"),">",file.path(bed.dir,"nonXLNAD_merged.bed"))
+  system(cmd) 
+  cmd = paste("bedops -d",ref.bed,file.path(bed.dir,"nonXLNAD_merged.bed"),">",file.path(bed.dir,"nonXL_nonNAD.bed"))
+  system(cmd)
+  
+  peaks=read.table(file.path(bed.dir,"nonXLNAD_merged.bed"))
+  colnames(peaks)[1:3]= c("chr","start","end")
+  nonXLNAD=toGRanges(peaks)
+  
+  peaks=read.table(file.path(bed.dir,"nonXL_nonNAD.bed"))
+  colnames(peaks)[1:3]= c("chr","start","end")
+  nonXL_nonNAD=toGRanges(peaks)
+  
+  XLNAD.MB <- sum(width(XLNAD))/1000000
+  XL_nonNAD.MB <- sum(width(XL_nonNAD))/1000000
+  
+  nonXLNAD.MB <- sum(width(nonXLNAD))/1000000
+  nonXL_nonNAD.MB <- sum(width(nonXL_nonNAD))/1000000
+  
+  
+  # export(re.out[[6]],file.path(bed.dir,"nonXL.bed"),format="BED")
+  # export(re.out[[8]],file.path(bed.dir,"XL.bed"),format="BED")
+  # 
+  # nad.bed <- paste(file.path(bed.dir,"nonXL.bed"),file.path(bed.dir,"XL.bed"),sep= " ")
+  # cmd = paste("bedops -u",nad.bed,">",file.path(bed.dir,"NAD.bed"))
+  # system(cmd)
+  # cmd = paste("bedtools sort -i",file.path(bed.dir,"NAD.bed"),">",file.path(bed.dir,"NAD_sorted.bed"))
+  # system(cmd) 
+  # cmd = paste("bedtools merge -i",file.path(bed.dir,"NAD_sorted.bed"),">",file.path(bed.dir,"NAD_merged.bed"))
+  # system(cmd) 
+  # peaks=read.table(file.path(bed.dir,"NAD_merged.bed"))
+  # colnames(peaks)[1:3]= c("chr","start","end")
+  # NAD=toGRanges(peaks)
+  # 
+  # # NAD.before <- unique(c(re.out[[6]],re.out[[8]]))
+  # 
+  # XLNAD <- re.out[[8]]
+  # nonXLNAD <- re.out[[6]]
+  #   
+  # # export(NAD,file.path(bed.dir,"NAD.bed"),format="BED")
+  # 
+  # 
+  # cmd = paste("bedops -d",ref.bed,file.path(bed.dir,"NAD_merged.bed"),">",file.path(bed.dir,"nonNAD.bed"))
+  # 
+  # system(cmd)
+  # 
+  # peaks=read.table(file.path(bed.dir,"nonNAD.bed"))
+  # 
+  # colnames(peaks)[1:3]= c("chr","start","end")
+  # nonNAD=toGRanges(peaks)
+  # 
+  # #mcols(NAD) <- NULL
+  # #nonNAD <- setdiff(gr.whole.genome,NAD)
+  # 
+  # XLNAD.MB <- sum(width(XLNAD))/1000000
+  # nonXLNAD.MB <- sum(width(nonXLNAD))/1000000
+  # 
+  # NAD.MB <- sum(width(NAD))/1000000
+  # nonNAD.MB <- sum(width(nonNAD))/1000000
   
   mbl <- lapply(1:length(XL.nonXL.subset),function(u,XL.nonXL.subset){
     
@@ -8492,15 +8559,38 @@ getGeneDensityPlot <- function() {
     mb
   },XL.nonXL.subset)
   names(mbl) <- names(XL.nonXL.subset)
-  names(mbl) <- unique(as.character(YYYY$SetName))[1:6]
+  #names(mbl) <- unique(as.character(YYYY$SetName))[1:6]
   
   mbl2 <- do.call(rbind,mbl)
   colnames(mbl2) <- "MB"
   
-  mbl3 <- as.data.frame(c(NAD.MB,nonNAD.MB,whole.genome.MB))
-  row.names(mbl3) <- unique(as.character(YYYY$SetName))[7:9]
+  mbl3 <- as.data.frame(c(XLNAD.MB,XL_nonNAD.MB,nonXLNAD.MB,nonXL_nonNAD.MB,whole.genome.MB))
+ 
+  
+  
+  row.names(mbl3) <- c("XLNAD.MB","XL_nonNAD.MB","nonXLNAD.MB","nonXL_nonNAD.MB","whole.genome.MB")
   colnames(mbl3) <- "MB"
   mbl4 <- rbind.data.frame(mbl2,mbl3)
+  
+  YYYYY <- YYYY
+    
+  YYYY.wholeGenome <- YYYY[which(YYYY$SetName %in% c("wholeGenome")),]
+  
+  YYYY.XLNAD <- YYYY[which(YYYY$SetName %in% c("XL_Uniq","XL_ciLAD","XL_LAD")),]
+  YYYY.XLNAD.3.class <- YYYY.XLNAD
+  YYYY.XLNAD$SetName <- "XLNAD" 
+    
+  YYYY.XL_nonNAD <- YYYY.wholeGenome[-which(YYYY.wholeGenome$GeneName %in% YYYY.XLNAD$GeneName),]
+  YYYY.XL_nonNAD$SetName <- "XL_nonNAD" 
+  
+  YYYY.nonXLNAD <- YYYY[which(YYYY$SetName %in% c("nonXL_Uniq","nonXL_ciLAD","nonXL_LAD")),]
+  YYYY.nonXLNAD.3.class <- YYYY.nonXLNAD
+  YYYY.nonXLNAD$SetName <- "nonXLNAD" 
+  
+  YYYY.nonXL_nonNAD <- YYYY.wholeGenome[-which(YYYY.wholeGenome$GeneName %in% YYYY.nonXLNAD$GeneName),]
+  YYYY.nonXL_nonNAD$SetName <- "nonXL_nonNAD"
+  
+  YYYY <- rbind.data.frame(YYYY.XLNAD.3.class,YYYY.XLNAD,YYYY.XL_nonNAD,YYYY.nonXLNAD.3.class,YYYY.nonXLNAD,YYYY.nonXL_nonNAD,YYYY.wholeGenome)
   
   num.gene <- lapply(unique(as.character(YYYY$SetName)), function(u,YYYY){
     num.gene <- length(unique(as.character(YYYY[which(YYYY$SetName==u),]$GeneName)))
@@ -8508,23 +8598,60 @@ getGeneDensityPlot <- function() {
   },YYYY)
   names(num.gene) <- unique(as.character(YYYY$SetName))
   num.gene.1 <- do.call(rbind,num.gene)
+  colnames(num.gene.1) <- "num"
   
-  mb.gene.num <- cbind(mbl4,num.gene.1,num.gene.1/mbl4)
+  row.names(mbl4) <- gsub(".MB","",row.names(mbl4))
+  row.names(mbl4) <- gsub("whole.genome","wholeGenome",row.names(mbl4))
   
-  mb.gene.num <- data.frame(mb.gene.num,Category=row.names(mb.gene.num))
+  mbl5 <- merge(mbl4,num.gene.1,by=0)
+  
+  #XLNAD.num.gene <- length(unique(as.character(YYYY[which(YYYY$SetName %in% c("XL_Uniq","XL_ciLAD","XL_LAD")),]$GeneName)))
+  #nonXLNAD.num.gene <- length(unique(as.character(YYYY[which(YYYY$SetName %in% c("nonXL_Uniq","nonXL_ciLAD","nonXL_LAD")),]$GeneName)))
+  
+  #XLNAD.nonXLNAD.num.gene <- as.data.frame(c(XLNAD.num.gene,nonXLNAD.num.gene))
+  #colnames(XLNAD.nonXLNAD.num.gene) <- "num"
+  #row.names(XLNAD.nonXLNAD.num.gene) <- c("XLNAD","nonXLNAD")
+  
+  #num.gene.1 <- rbind(num.gene.1,XLNAD.nonXLNAD.num.gene)
+  
+  
+  #mbl4[rownames(mbl4)=="XLNAD",] <- sum(mbl4[rownames(mbl4) %in% c("XL_Uniq","XL_ciLAD","XL_LAD"),])
+  
+  #mbl4[rownames(mbl4)=="nonXLNAD",] <- sum(mbl4[rownames(mbl4) %in% c("nonXL_Uniq","nonXL_ciLAD","nonXL_LAD"),])
+  
+  mb.gene.num <- cbind(mbl5$MB,mbl5$num,mbl5$num/mbl5$MB,mbl5$Row.names)
+  
+  #mb.gene.num <- data.frame(mb.gene.num,Category=row.names(mb.gene.num))
   
   colnames(mb.gene.num) <- c("MB","num.gene","Density","Category")
   
-  DF <- mb.gene.num
+  DF <- as.data.frame(mb.gene.num)
+  DF$MB <- as.numeric(as.character(DF$MB))
+  DF$num.gene <- as.numeric(as.character(DF$num.gene))
+  DF$Density <- as.numeric(as.character(DF$Density))
   
   #create a colour scheme based on grouping variable 'zone'
-  Category<- c("XL_Uniq","XL_ciLAD","XL_LAD","nonXL_Uniq","nonXL_ciLAD","nonXL_LAD","NAD","nonNAD","wholeGenome")
-  color.codes<-as.character(c("#3399FF", "#FF0000","#9633FF","#3399FF", "#FF0000","#9633FF","#0000FF","#C71585","#FFC0CB"))
+  Category<- as.character(DF$Category)
+  color.codes <- rainbow(length(Category))
+  
+  c.1 <- color.codes[which(Category %in% c("nonXL_Uniq","XL_Uniq"))][1]
+
+  color.codes[which(Category %in% c("nonXL_Uniq","XL_Uniq"))] <- c.1
+  
+  c.2 <- color.codes[which(Category %in% c("nonXL_ciLAD","XL_ciLAD"))][1]
+  color.codes[which(Category %in% c("nonXL_ciLAD","XL_ciLAD"))] <- c.2
+  
+  c.3 <- color.codes[which(Category %in% c("nonXL_LAD" ,"XL_LAD"))][1]
+  color.codes[which(Category %in% c("nonXL_LAD" ,"XL_LAD"))] <- c.3
+    
+  
+  #color.codes<-as.character(c("#3399FF", "#FF0000","#9633FF","#3399FF", "#FF0000","#9633FF","#0000FF","#C71585","#FFC0CB","#FF0000FF","#00FFFFFF"))
   
   df2=data.frame(Category, color.codes)
   
   # merge color specifications with data
   df <-merge(DF,df2, by=("Category"), all.x=TRUE, all.y=TRUE)
+  
   
   df$Category <- factor(df$Category, levels = df$Category[order(df$Density,decreasing = T)])
   
@@ -8541,7 +8668,6 @@ getGeneDensityPlot <- function() {
   
   write.table(df[,1:4],file = file.path(bed.dir,paste0("Density",".txt")),
               append = FALSE, quote = F, sep = "\t",eol = "\n", na = "NA", dec = ".", row.names = F,col.names = T)
-  
   
 }
 
