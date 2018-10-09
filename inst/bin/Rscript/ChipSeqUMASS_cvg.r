868ed3b4a53910c0e55b7dcebc46da16b093c9ba
Aimin Yan <aimin.at.work@gmail.com>
2018-10-09 17:29:48 -0400

    add _cvg.r

---
+++ inst/bin/Rscript/ChipSeqUMASS_cvg.r
@@ -0,0 +1,56 @@
+#!/usr/bin/env Rscript
+
+# source("https://bioconductor.org/biocLite.R")
+# biocLite("BSgenome.Mmusculus.UCSC.mm10")
+
+packages <- c("optparse","ChIPpeakAnno","ChIPseeker","EnsDb.Mmusculus.v75","DBI","tidyverse","annotate",
+              "reactome.db","dplyr","ggplot2","ggpubr","stringr","grid","Vennerable","devtools","reshape2",
+              "binaryLogic","org.Hs.eg.db","org.Mm.eg.db","eulerr","DiffBind","LOLA",
+              "TxDb.Hsapiens.UCSC.hg38.knownGene","EnsDb.Hsapiens.v75","BSgenome.Hsapiens.UCSC.hg19",
+              "BSgenome.Mmusculus.UCSC.mm10","rtracklayer")
+
+null<-lapply(packages, function(xxx) suppressMessages(require(xxx, character.only = TRUE,quietly=TRUE,warn.conflicts = FALSE)))
+
+option_list = list(make_option(c("-i", "--input_file_dir"), type="character", default=NULL,
+                               help="Input file dir", metavar="character"),
+                   make_option(c("-w", "--input_bw_file_dir"), type="character", default=NULL,
+                               help="Input BigWig files", metavar="character"),
+                   ake_option(c("-o", "--out_dir"), type="character", default=".",
+                               help="output fir[default= %default]", metavar="character")
+);
+
+example.use <- 'Example:
+Rscript $HOME/Aimin/HomeAtCluster/Project/ChipSeqUMASS/inst/bin/Rscript/ChipSeqUMASS.r -i ~/Aimin/DropboxUmass/NADfinder/BedFiles -p ".bed$" -g "Mm" -o ~/Aimin/DropboxUmass/NADfinder/Aimin/Output'
+
+example.use <- 'Example: Rscript /home/ay64w/Project/ChipSeqUMASS/inst/bin/Rscript/ChipSeqUMASS.r -i /project/umw_nathan_lawson/toDoForAVpaper/1_DensityPlots/bedFilesforDensityPlots/byClass -w /project/umw_nathan_lawson/toDoForAVpaper/1_DensityPlots/H3K27me3_bigwig -o /project/umw_nathan_lawson/toDoForAVpaper/1_DensityPlots/H3K27me3_cvg_2'
+
+opt_parser = OptionParser(option_list=option_list,epilogue=example.use);
+opt = parse_args(opt_parser);
+
+if (is.null(opt$input_file_dir)){
+  print_help(opt_parser)
+  stop("At least one argument must be supplied (input file dir)", call.=FALSE)
+}
+
+input.file.dir <- opt$input_file_dir
+input.bw.file <- opt$input_bw_file_dir
+out.dir <- opt$out_dir
+
+if(!dir.exists(out.dir)){dir.create(out.dir,recursive = TRUE)}
+
+initial.options <- commandArgs(trailingOnly = FALSE)
+file.arg.name <- "--file="
+script.name <- sub(file.arg.name, "", initial.options[grep(file.arg.name, initial.options)])
+script.basename <- dirname(dirname(dirname(dirname(script.name))))
+other.name <- file.path(script.basename, "R/ChipSeq.R")
+print(paste("Sourcing",other.name,"from",script.name))
+source(other.name)
+
+print(input.file.dir)
+print(input.bw.file)
+print(out.dir)
+
+#AnnotatePeakUMASS(input.file.dir,input.file.pattern,out.dir,genome=input.genome)
+
+#getCvg(input.file.dir,out.dir)
+overLapWithOtherFeatures(input.file.dir,input.bw.file,out.dir)
