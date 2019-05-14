975ed81b4e43e43920d853f6ac104c1a42d9aa0b
Aimin Yan <aimin.at.work@gmail.com>
2019-05-14 16:58:20 -0400

    change docs

--- R/newFunction2Add.R
+++ R/newFunction2Add.R
@@ -1,5 +1,4 @@
-# Aimin, here are a few functions that I can think of right now.
-
+# function to get the overlapping region 
 intersectBED <- function(inputGRangeList, byBase = TRUE, output = c("BED", "WIG"), outputFileNamePrefix = "common"){
   
   
@@ -10,7 +9,7 @@ uniqueBED <- function(inputGRangeList, byBase = TRUE, output  = c("BED", "WIG"),
   
   
 }
-                      
+
 # The updated function for drawing heatmap around TSS that you mentioned to me previously
 
 # Plot Jarcard heatmap with flexible parameters: 
@@ -20,8 +19,7 @@ plotHeatMapOfJI <- function(jarcardIndexMatrix, …){
 }
 
 plotAggregatedSignalsAroundPeaks <- function(peakGRangeList, upstream = 5000, downstream = 3000, combineSamples = TRUE, combineSamplesBy = c(mean, median), …)
-                            
-getDomainSplittingRegions <- function(domainGRangesWithSRs, domainGRangelistWithoutSRs) ### For example, NADs from NADfinder can be used as domainGRangesWithSRs,  NADs from MACs, normR and EDD can be used as domainGRangelistWithoutSRs, the output should be NSRs (NAD splitting regions) in this example. We name DSRs to be more general.
-                          
-# Please let us know if there are other functions you can think of that would be appropriate to be added to ChIPpeakAnno. Thanks!
+       
+# For example, NADs from NADfinder can be used as domainGRangesWithSRs,  NADs from MACs, normR and EDD can be used as domainGRangelistWithoutSRs, the output should be NSRs (NAD splitting regions) in this example. We name DSRs to be more general.                     
+getDomainSplittingRegions <- function(domainGRangesWithSRs, domainGRangelistWithoutSRs)
 
