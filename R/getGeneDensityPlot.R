72a00618c01323858c8e9f729ed29bb9a71f0124
Aimin Yan <aimin.at.work@gmail.com>
2019-06-17 12:26:31 -0400

    remove getGeneDensityPlot2 function

--- R/getGeneDensityPlot.R
+++ R/getGeneDensityPlot.R
@@ -1,7 +1,83 @@
-# bed.dir <- "/Users/aiminyan/Aimin/DropboxUmass/NADfinder/Aimin/Results_10_4_2018_gene_density"
+#!/usr/bin/env Rscript
 
-# bed.dir <- "/Users/aiminyan/Aimin/DropboxUmass/NADfinder/Aimin/Results_10_7_2018_gene_density"
-# getGeneDensityPlot(bed.dir)
+# 1) F121-9 NADs
+# Dropbox/pub_old_galaxy/F121-9/avesig filtered NADs/F121_9_all_1.5FC_F200_minp_adjpVal_avesig_greater_than_1.7_NAD_greater_50kb.bed
+
+F121.9.NADs <- "/Users/aiminyan/Aimin/DropboxUmass/pub_old_galaxy/F121-9/avesig filtered NADs/F121_9_all_1.5FC_F200_minp_adjpVal_avesig_greater_than_1.7_NAD_greater_50kb.bed"
+
+# 2) F121-9 Type I NADs
+# Dropbox\pub_old_galaxy\F121-9\overlap analysis\F121-9 vs CLAD vs ciLAD\common-in-F121_9_and_cLAD.bed
+
+F121.9.Type.I.NADs <- "/Users/aiminyan/Aimin/DropboxUmass/pub_old_galaxy/F121-9/overlap analysis/F121-9 vs CLAD vs ciLAD/common-in-F121_9_and_cLAD.bed"
+
+# 3) F121-9 Type II NADs
+# Dropbox\pub_old_galaxy\F121-9\overlap analysis\F121-9 vs CLAD vs ciLAD\common-in-F121_9_and_ciLAD.bed
+
+F121.9.Type.II.NADs <- "/Users/aiminyan/Aimin/DropboxUmass/pub_old_galaxy/F121-9/overlap analysis/F121-9 vs CLAD vs ciLAD/common-in-F121_9_and_ciLAD.bed"
+
+# 4) Shared NADs
+# Dropbox\pub_old_galaxy\F121-9\overlap analysis\F121-9 vs MEF XL NADs\common-in-F121_9_and-XL_MEF.bed
+
+shared.NADs <- "/Users/aiminyan/Aimin/DropboxUmass/pub_old_galaxy/F121-9/overlap analysis/F121-9 vs MEF XL NADs/common-in-F121_9_and-XL_MEF.bed"
+
+# 5) F121-9 specific NADs
+# Dropbox\pub_old_galaxy\F121-9\overlap analysis\F121-9 vs MEF XL NADs\unique-F121_9.bed
+
+F121.9.specific.NADs <- "/Users/aiminyan/Aimin/DropboxUmass/pub_old_galaxy/F121-9/overlap analysis/F121-9 vs MEF XL NADs/unique-F121_9.bed"
+
+# 6) MEF specific NADs
+# Dropbox\pub_old_galaxy\F121-9\overlap analysis\F121-9 vs MEF XL NADs\unique-XL_MEF.bed
+
+MEF.specific.NADs <- "/Users/aiminyan/Aimin/DropboxUmass/pub_old_galaxy/F121-9/overlap analysis/F121-9 vs MEF XL NADs/unique-XL_MEF.bed"
+
+# 7) cLAD
+# Dropbox/NADfinder/BedFiles/cLAD.mm10 bed with header.bed
+
+cLAD <- "/Users/aiminyan/Aimin/DropboxUmass/NADfinder/BedFiles/cLAD.mm10 bed with header.bed"
+
+# 8) ciLAD
+# Dropbox/NADfinder/BedFiles/ciLAD.mm10 bed with header.bed
+
+ciLAD <- "/Users/aiminyan/Aimin/DropboxUmass/NADfinder/BedFiles/ciLAD.mm10 bed with header.bed"
+
+# 9) Early replication timing: all regions above zero in this bedgraph 
+# Dropbox (UMass Medical School)\pub_old_galaxy\F121-9\published data\GSE95091_F121-9_nesc_r1_R_T_.bedGraph
+
+Early.replication.timing <- "/Users/aiminyan/Aimin/DropboxUmass/pub_old_galaxy/F121-9/published data/GSE95091_F121-9_nesc_r1_R_T_.bedGraph"
+
+# 10) Late replication timing: all regions below zero in this bedgraph
+# Dropbox (UMass Medical School)\pub_old_galaxy\F121-9\published data\GSE95091_F121-9_nesc_r1_R_T_.bedGraph
+
+Late.replication.timing <- "/Users/aiminyan/Aimin/DropboxUmass/pub_old_galaxy/F121-9/published data/GSE95091_F121-9_nesc_r1_R_T_.bedGraph"
+
+H3K27me3 <- "/Users/aiminyan/Aimin/DropboxUmass/NADfinder/Aimin/H3K27me3/GSM2416833_WT_2i_ChIP_H3K27me3-domains-1.bed"
+
+F121_9_nonNADs <- "/Users/aiminyan/Aimin/DropboxUmass/NADfinder/Aimin/Output4_F121_9_Type_I_NADs_F121_9_Type_II_NADs_F121_9_NADs_F121_9_nonNADs_box_plot/F121_9_nonNADs.bed"
+
+
+output.file.dir <- "/Users/aiminyan/Aimin/DropboxUmass/NADfinder/Aimin/Output4AizhanJaccardAnalysis"
+
+bed.files <- c(F121.9.NADs,F121.9.Type.I.NADs,F121.9.Type.II.NADs,shared.NADs,F121.9.specific.NADs,MEF.specific.NADs,cLAD,ciLAD,Early.replication.timing,Late.replication.timing,H3K27me3,F121_9_nonNADs)
+
+bed.in<-lapply(1:length(bed.files),function(u,bed.files){
+  
+  x <- bed.files[[u]]
+  
+  if(u == 2|u==3|u==4|u==5|u==6|u==9|u==10|u==11|u==12){peaks=read.table(x,header=F)}else{peaks=read.table(x,header=F,skip=1)}
+  
+  colnames(peaks)[1:3]= c("chr","start","end")
+  peaks=toGRanges(peaks)
+  peaks
+},bed.files)
+
+names(bed.in) <- c("F121.9.NADs","F121.9.Type.I.NADs","F121.9.Type.II.NADs","shared.NADs","F121.9.specific.NADs","MEF.specific.NADs","cLAD","ciLAD","Early.replication.timing","Late.replication.timing","H3K27me3","F121_9_nonNADs")
+
+Ert <- bed.in[[9]][which(bed.in[[9]]$V4>0)]
+Lrt <- bed.in[[10]][which(bed.in[[10]]$V4<0)]
+
+bed.in[[9]] <- Ert
+bed.in[[10]] <- Lrt
+
 
 names(bed.in[c(12,3,1,2)])
 
@@ -27,10 +103,24 @@ Gr2DfWithMB <- function(XL.nonXL.subset) {
 
 bed.4.gene.density.1.df <- Gr2DfWithMB(bed.4.gene.density.1)
 
-load("/Users/aiminyan/Aimin/DropboxUmass/NADfinder/Aimin/Output4Aizhan_F121_9_avesig_filtered_NADsVenn/F121-9_box_plot/F121-9_box_plot.RData")
+bed.dir <- "/Users/aiminyan/Aimin/DropboxUmass/NADfinder/Aimin/Output4AizhanGeneDensity"
+
+rna.seq.data.from.Aizhan <- readRDS(file=file.path(bed.dir,"rnaSeq_data_for_F121_9_FPKM.rds"))
+
+#load("/Users/aiminyan/Aimin/DropboxUmass/NADfinder/Aimin/Output4Aizhan_F121_9_avesig_filtered_NADsVenn/F121-9_box_plot/F121-9_box_plot.RData")
 
 bed.4.gene.density.1.anno <- getAnnotatedGene(bed.4.gene.density.1,"Mm")
 bed.4.gene.density.1.anno.YYY <- getFPKM4DiffSet(bed.4.gene.density.1.anno,rna.seq.data.from.Aizhan)
+
+getFPKMboxplot <- function(bed.dir,YYYY) {
+  
+  png(file = file.path(bed.dir,paste0(paste(levels(YYYY$SetName),collapse = "-"),".png")),width = 1500, height = 480)
+  boxplot(log10(as.numeric(as.character(YYYY$FPKM))+1)~YYYY$SetName,ylab = "log10(FPKM+1)")
+  dev.off()
+
+}
+
+getFPKMboxplot(bed.dir,bed.4.gene.density.1.anno.YYY)
 
 getNumOfGene4Sets <- function(YYYY) {
   num.gene <- lapply(unique(as.character(YYYY$SetName)), function(u,YYYY){
@@ -75,240 +165,13 @@ sp <- ggplot(df, aes(x = Category, y = Density, fill = color.codes)) +
 spp <- list(sp=sp)
 multi.page <- ggarrange(plotlist=spp,nrow = 1, ncol = 1)
 
-bed.dir <- "/Users/aiminyan/Aimin/DropboxUmass/NADfinder/Aimin/Output4AizhanGeneDensity"
-
 if(!dir.exists(bed.dir)){dir.create(bed.dir,recursive = TRUE)}
 ggexport(multi.page, width = 1000, height = 480,filename = file.path(bed.dir,"GeneDensity.png"))
 
 write.table(df[,1:4],file = file.path(bed.dir,paste0("Density",".txt")),
             append = FALSE, quote = F, sep = "\t",eol = "\n", na = "NA", dec = ".", row.names = F,col.names = T)
 
+saveRDS(bed.in,file = file.path(bed.dir,paste0(paste(names(bed.in),collapse = "-"),".rds")))
+save.image(file = file.path(bed.dir,paste0(basename(bed.dir),".RData")))
+savehistory(file = file.path(bed.dir,paste0(basename(bed.dir),".Rhistory")))
 
-
-getGeneDensityPlot2 <- function(bed.dir) {
-  
-  if(!dir.exists(bed.dir)){dir.create(bed.dir,recursive = TRUE)}
-  # whole genome in MB
-  whole.genome.MB <- sum(width(gr.whole.genome))/1000000
-  
-  names(XL.nonXL.subset) <- c("XL_Uniq","XL_ciLAD","XL_LAD","nonXL_Uniq","nonXL_ciLAD","nonXL_LAD")
-  
-  null <- lapply(1:length(XL.nonXL.subset), function(u,XL.nonXL.subset){
-    
-    x <- names(XL.nonXL.subset)[u] 
-    export(XL.nonXL.subset[[u]],file.path(bed.dir,paste0(x,".bed")),format="BED")
-    
-  },XL.nonXL.subset)
-  
-  xl.nad.bed <- file.path(bed.dir,paste0(c("XL_Uniq","XL_ciLAD","XL_LAD"),".bed"))
-  cmd = paste("bedops -u",paste(xl.nad.bed,collapse = " "),">",file.path(bed.dir,"XLNAD.bed"))
-  system(cmd)
-  cmd = paste("bedtools sort -i",file.path(bed.dir,"XLNAD.bed"),">",file.path(bed.dir,"XLNAD_sorted.bed"))
-  system(cmd) 
-  cmd = paste("bedtools merge -i",file.path(bed.dir,"XLNAD_sorted.bed"),">",file.path(bed.dir,"XLNAD_merged.bed"))
-  system(cmd) 
-  cmd = paste("bedops -d",ref.bed,file.path(bed.dir,"XLNAD_merged.bed"),">",file.path(bed.dir,"XL_nonNAD.bed"))
-  system(cmd)
-  
-  
-  peaks=read.table(file.path(bed.dir,"XLNAD_merged.bed"))
-  colnames(peaks)[1:3]= c("chr","start","end")
-  XLNAD=toGRanges(peaks)
-  
-  peaks=read.table(file.path(bed.dir,"XL_nonNAD.bed"))
-  colnames(peaks)[1:3]= c("chr","start","end")
-  XL_nonNAD=toGRanges(peaks)
-  
-  
-  
-  nonxl.nad.bed <- file.path(bed.dir,paste0(c("nonXL_Uniq","nonXL_ciLAD","nonXL_LAD"),".bed"))
-  cmd = paste("bedops -u",paste(nonxl.nad.bed,collapse = " "),">",file.path(bed.dir,"nonXLNAD.bed"))
-  system(cmd)
-  cmd = paste("bedtools sort -i",file.path(bed.dir,"nonXLNAD.bed"),">",file.path(bed.dir,"nonXLNAD_sorted.bed"))
-  system(cmd) 
-  cmd = paste("bedtools merge -i",file.path(bed.dir,"nonXLNAD_sorted.bed"),">",file.path(bed.dir,"nonXLNAD_merged.bed"))
-  system(cmd) 
-  cmd = paste("bedops -d",ref.bed,file.path(bed.dir,"nonXLNAD_merged.bed"),">",file.path(bed.dir,"nonXL_nonNAD.bed"))
-  system(cmd)
-  
-  peaks=read.table(file.path(bed.dir,"nonXLNAD_merged.bed"))
-  colnames(peaks)[1:3]= c("chr","start","end")
-  nonXLNAD=toGRanges(peaks)
-  
-  peaks=read.table(file.path(bed.dir,"nonXL_nonNAD.bed"))
-  colnames(peaks)[1:3]= c("chr","start","end")
-  nonXL_nonNAD=toGRanges(peaks)
-  
-  XLNAD.MB <- sum(width(XLNAD))/1000000
-  XL_nonNAD.MB <- sum(width(XL_nonNAD))/1000000
-  
-  nonXLNAD.MB <- sum(width(nonXLNAD))/1000000
-  nonXL_nonNAD.MB <- sum(width(nonXL_nonNAD))/1000000
-  
-  
-  # export(re.out[[6]],file.path(bed.dir,"nonXL.bed"),format="BED")
-  # export(re.out[[8]],file.path(bed.dir,"XL.bed"),format="BED")
-  # 
-  # nad.bed <- paste(file.path(bed.dir,"nonXL.bed"),file.path(bed.dir,"XL.bed"),sep= " ")
-  # cmd = paste("bedops -u",nad.bed,">",file.path(bed.dir,"NAD.bed"))
-  # system(cmd)
-  # cmd = paste("bedtools sort -i",file.path(bed.dir,"NAD.bed"),">",file.path(bed.dir,"NAD_sorted.bed"))
-  # system(cmd) 
-  # cmd = paste("bedtools merge -i",file.path(bed.dir,"NAD_sorted.bed"),">",file.path(bed.dir,"NAD_merged.bed"))
-  # system(cmd) 
-  # peaks=read.table(file.path(bed.dir,"NAD_merged.bed"))
-  # colnames(peaks)[1:3]= c("chr","start","end")
-  # NAD=toGRanges(peaks)
-  # 
-  # # NAD.before <- unique(c(re.out[[6]],re.out[[8]]))
-  # 
-  # XLNAD <- re.out[[8]]
-  # nonXLNAD <- re.out[[6]]
-  #   
-  # # export(NAD,file.path(bed.dir,"NAD.bed"),format="BED")
-  # 
-  # 
-  # cmd = paste("bedops -d",ref.bed,file.path(bed.dir,"NAD_merged.bed"),">",file.path(bed.dir,"nonNAD.bed"))
-  # 
-  # system(cmd)
-  # 
-  # peaks=read.table(file.path(bed.dir,"nonNAD.bed"))
-  # 
-  # colnames(peaks)[1:3]= c("chr","start","end")
-  # nonNAD=toGRanges(peaks)
-  # 
-  # #mcols(NAD) <- NULL
-  # #nonNAD <- setdiff(gr.whole.genome,NAD)
-  # 
-  # XLNAD.MB <- sum(width(XLNAD))/1000000
-  # nonXLNAD.MB <- sum(width(nonXLNAD))/1000000
-  # 
-  # NAD.MB <- sum(width(NAD))/1000000
-  # nonNAD.MB <- sum(width(nonNAD))/1000000
-  
-  mbl <- lapply(1:length(XL.nonXL.subset),function(u,XL.nonXL.subset){
-    
-    mb <- sum(width(XL.nonXL.subset[[u]]))/1000000
-    mb
-  },XL.nonXL.subset)
-  names(mbl) <- names(XL.nonXL.subset)
-  #names(mbl) <- unique(as.character(YYYY$SetName))[1:6]
-  
-  mbl2 <- do.call(rbind,mbl)
-  colnames(mbl2) <- "MB"
-  
-  mbl3 <- as.data.frame(c(XLNAD.MB,XL_nonNAD.MB,nonXLNAD.MB,nonXL_nonNAD.MB,whole.genome.MB))
-  
-  
-  
-  row.names(mbl3) <- c("XLNAD.MB","XL_nonNAD.MB","nonXLNAD.MB","nonXL_nonNAD.MB","whole.genome.MB")
-  colnames(mbl3) <- "MB"
-  mbl4 <- rbind.data.frame(mbl2,mbl3)
-  
-  YYYYY <- YYYY
-  
-  YYYY.wholeGenome <- YYYY[which(YYYY$SetName %in% c("wholeGenome")),]
-  
-  YYYY.XLNAD <- YYYY[which(YYYY$SetName %in% c("XL_Uniq","XL_ciLAD","XL_LAD")),]
-  YYYY.XLNAD.3.class <- YYYY.XLNAD
-  YYYY.XLNAD$SetName <- "XLNAD" 
-  
-  YYYY.XL_nonNAD <- YYYY.wholeGenome[-which(YYYY.wholeGenome$GeneName %in% YYYY.XLNAD$GeneName),]
-  YYYY.XL_nonNAD$SetName <- "XL_nonNAD" 
-  
-  YYYY.nonXLNAD <- YYYY[which(YYYY$SetName %in% c("nonXL_Uniq","nonXL_ciLAD","nonXL_LAD")),]
-  YYYY.nonXLNAD.3.class <- YYYY.nonXLNAD
-  YYYY.nonXLNAD$SetName <- "nonXLNAD" 
-  
-  YYYY.nonXL_nonNAD <- YYYY.wholeGenome[-which(YYYY.wholeGenome$GeneName %in% YYYY.nonXLNAD$GeneName),]
-  YYYY.nonXL_nonNAD$SetName <- "nonXL_nonNAD"
-  
-  YYYY <- rbind.data.frame(YYYY.XLNAD.3.class,YYYY.XLNAD,YYYY.XL_nonNAD,YYYY.nonXLNAD.3.class,YYYY.nonXLNAD,YYYY.nonXL_nonNAD,YYYY.wholeGenome)
-  
-  YYYY$SetName <- droplevels(YYYY$SetName)
-  
-  png(file = file.path(bed.dir,paste0(paste(levels(YYYY$SetName),collapse = "-"),".png")),width = 1500, height = 480)
-  boxplot(log10(as.numeric(as.character(YYYY$FPKM))+1)~YYYY$SetName,ylab = "log10(FPKM+1)")
-  dev.off()
-  
-  
-  num.gene <- lapply(unique(as.character(YYYY$SetName)), function(u,YYYY){
-    num.gene <- length(unique(as.character(YYYY[which(YYYY$SetName==u),]$GeneName)))
-    num.gene
-  },YYYY)
-  names(num.gene) <- unique(as.character(YYYY$SetName))
-  num.gene.1 <- do.call(rbind,num.gene)
-  colnames(num.gene.1) <- "num"
-  
-  row.names(mbl4) <- gsub(".MB","",row.names(mbl4))
-  row.names(mbl4) <- gsub("whole.genome","wholeGenome",row.names(mbl4))
-  
-  mbl5 <- merge(mbl4,num.gene.1,by=0)
-  
-  #XLNAD.num.gene <- length(unique(as.character(YYYY[which(YYYY$SetName %in% c("XL_Uniq","XL_ciLAD","XL_LAD")),]$GeneName)))
-  #nonXLNAD.num.gene <- length(unique(as.character(YYYY[which(YYYY$SetName %in% c("nonXL_Uniq","nonXL_ciLAD","nonXL_LAD")),]$GeneName)))
-  
-  #XLNAD.nonXLNAD.num.gene <- as.data.frame(c(XLNAD.num.gene,nonXLNAD.num.gene))
-  #colnames(XLNAD.nonXLNAD.num.gene) <- "num"
-  #row.names(XLNAD.nonXLNAD.num.gene) <- c("XLNAD","nonXLNAD")
-  
-  #num.gene.1 <- rbind(num.gene.1,XLNAD.nonXLNAD.num.gene)
-  
-  
-  #mbl4[rownames(mbl4)=="XLNAD",] <- sum(mbl4[rownames(mbl4) %in% c("XL_Uniq","XL_ciLAD","XL_LAD"),])
-  
-  #mbl4[rownames(mbl4)=="nonXLNAD",] <- sum(mbl4[rownames(mbl4) %in% c("nonXL_Uniq","nonXL_ciLAD","nonXL_LAD"),])
-  
-  mb.gene.num <- cbind(mbl5$MB,mbl5$num,mbl5$num/mbl5$MB,mbl5$Row.names)
-  
-  #mb.gene.num <- data.frame(mb.gene.num,Category=row.names(mb.gene.num))
-  
-  colnames(mb.gene.num) <- c("MB","num.gene","Density","Category")
-  
-  DF <- as.data.frame(mb.gene.num)
-  DF$MB <- as.numeric(as.character(DF$MB))
-  DF$num.gene <- as.numeric(as.character(DF$num.gene))
-  DF$Density <- as.numeric(as.character(DF$Density))
-  
-  #create a colour scheme based on grouping variable 'zone'
-  Category<- as.character(DF$Category)
-  color.codes <- rainbow(length(Category))
-  
-  c.1 <- color.codes[which(Category %in% c("nonXL_Uniq","XL_Uniq"))][1]
-  
-  color.codes[which(Category %in% c("nonXL_Uniq","XL_Uniq"))] <- c.1
-  
-  c.2 <- color.codes[which(Category %in% c("nonXL_ciLAD","XL_ciLAD"))][1]
-  color.codes[which(Category %in% c("nonXL_ciLAD","XL_ciLAD"))] <- c.2
-  
-  c.3 <- color.codes[which(Category %in% c("nonXL_LAD" ,"XL_LAD"))][1]
-  color.codes[which(Category %in% c("nonXL_LAD" ,"XL_LAD"))] <- c.3
-  
-  c.4 <- color.codes[which(Category %in% c("nonXLNAD" ,"XLNAD"))][1]
-  color.codes[which(Category %in% c("nonXLNAD" ,"XLNAD"))] <- c.4
-  
-  #color.codes<-as.character(c("#3399FF", "#FF0000","#9633FF","#3399FF", "#FF0000","#9633FF","#0000FF","#C71585","#FFC0CB","#FF0000FF","#00FFFFFF"))
-  
-  df2=data.frame(Category, color.codes)
-  
-  # merge color specifications with data
-  df <-merge(DF,df2, by=("Category"), all.x=TRUE, all.y=TRUE)
-  
-  
-  df$Category <- factor(df$Category, levels = df$Category[order(df$Density,decreasing = T)])
-  
-  sp <- ggplot(df, aes(x = Category, y = Density, fill = color.codes)) +
-    geom_bar(stat="identity",position = "dodge")  +
-    theme(plot.title=element_text(hjust=0.5)) +  
-    ylab("Gene density (# of genes/MB)")  +  theme(legend.position="none")
-  
-  spp <- list(sp=sp)
-  multi.page <- ggarrange(plotlist=spp,nrow = 1, ncol = 1)
-  
-  if(!dir.exists(bed.dir)){dir.create(bed.dir,recursive = TRUE)}
-  ggexport(multi.page, width = 1000, height = 480,filename = file.path(bed.dir,"GeneDensity.png"))
-  
-  write.table(df[,1:4],file = file.path(bed.dir,paste0("Density",".txt")),
-              append = FALSE, quote = F, sep = "\t",eol = "\n", na = "NA", dec = ".", row.names = F,col.names = T)
-  
-}
