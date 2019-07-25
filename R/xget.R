b8ccb6b041ceb080600683ed4f8063ec20067b43
Jianhong Ou <jianhongou@Jianhongs-MacBook-Pro.local>
2019-07-25 14:16:52 -0400

    add last choice for xget

--- R/xget.R
+++ R/xget.R
@@ -1,10 +1,11 @@
 xget <- function(x, envir, mode, ifnotfound=NA, inherits, 
-                 output=c("all", "first")){
+                 output=c("all", "first", "last")){
     output <- match.arg(output)
     y <- mget(x=x, envir=envir, mode=mode, 
               ifnotfound=ifnotfound, inherits=inherits)
     switch(output, 
            all=sapply(y, base::paste, collapse=";"),
-           first=sapply(y, `[`, 1)
+           first=sapply(y, `[`, 1),
+           last=sapply(y, function(.ele) .ele[length(.ele)])
     )
 }
